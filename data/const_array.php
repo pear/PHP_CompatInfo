68bdf97dca505fca2bdcb3ce1b366af235b63cbe
Davey Shafik <davey@php.net>
2005-02-21 07:24:50 +0000

    * Now contains all new PHP5 tokens
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CompatInfo/trunk@180299 c90b9560-bf6c-de11-be94-00142212c4b1

--- data/const_array.php
+++ data/const_array.php
@@ -1,17 +1,110 @@
 <?php
-$const['tokens'][] = T_FILE;
-$const['tokens'][] = T_LINE;
-$const['tokens'][] = T_FUNC_C;
-$const['tokens'][] = T_CLASS_C;
-$const['tokens'][] = T_METHOD_C;
-$const[T_FILE]['init'] = '4.0.0';
-$const[T_FILE]['name'] = '__FILE__';
-$const[T_LINE]['init'] = '4.0.0';
-$const[T_LINE]['name'] = '__LINE__';
-$const[T_FUNC_C]['init'] = '4.3.0';
-$const[T_FUNC_C]['name'] = '__FUNCTION__';
-$const[T_CLASS_C]['init'] = '4.3.0';
-$const[T_CLASS_C]['name'] = '__CLASS__';
-@$const[T_METHOD_C]['name'] = '__METHOD__';
-@$const[T_METHOD_C]['init'] = '5.0.0';
+$GLOBALS['const'] = array (
+  'tokens' => 
+  array (
+    0 => 363,
+    1 => 362,
+    2 => 361,
+    3 => 359,
+    4 => 360,
+    5 => 345,
+    6 => 337,
+    7 => 344,
+    8 => 288,
+    9 => 343,
+    10 => 342,
+    11 => 341,
+    12 => 338,
+    13 => 336,
+    14 => 298,
+    15 => 352,
+    16 => 354,
+  ),
+  363 => 
+  array (
+    'init' => '4.0.0',
+    'name' => '__FILE__',
+  ),
+  362 => 
+  array (
+    'init' => '4.0.0',
+    'name' => '__LINE__',
+  ),
+  361 => 
+  array (
+    'init' => '4.3.0',
+    'name' => '__FUNCTION__',
+  ),
+  359 => 
+  array (
+    'init' => '4.3.0',
+    'name' => '__CLASS__',
+  ),
+  360 => 
+  array (
+    'name' => '__METHOD__',
+    'init' => '5.0.0',
+  ),
+  345 => 
+  array (
+    'init' => '5.0.0',
+    'name' => 'abstract',
+  ),
+  337 => 
+  array (
+    'init' => '5.0.0',
+    'name' => 'catch',
+  ),
+  344 => 
+  array (
+    'init' => '5.0.0',
+    'name' => 'final',
+  ),
+  288 => 
+  array (
+    'init' => '5.0.0',
+    'name' => 'instanceof',
+  ),
+  343 => 
+  array (
+    'init' => '5.0.0',
+    'name' => 'private',
+  ),
+  342 => 
+  array (
+    'init' => '5.0.0',
+    'name' => 'protected',
+  ),
+  341 => 
+  array (
+    'init' => '5.0.0',
+    'name' => 'public',
+  ),
+  338 => 
+  array (
+    'init' => '5.0.0',
+    'name' => 'throw',
+  ),
+  336 => 
+  array (
+    'init' => '5.0.0',
+    'name' => 'try',
+  ),
+  298 => 
+  array (
+    'init' => '5.0.0',
+    'name' => 'clone',
+  ),
+  352 => 
+  array (
+    'init' => '5.0.0',
+    'name' => 'interface',
+  ),
+  354 => 
+  array (
+    'init' => '5.0.0',
+    'name' => 'implements',
+  ),
+);
+
 ?>
