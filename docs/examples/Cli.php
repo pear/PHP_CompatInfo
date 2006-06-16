bbcfbf8e8245c6371f39f8d71712bb162953c04f
Laurent Laville <farell@php.net>
2006-06-16 20:34:04 +0000

    add header comment block missing (QA)
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CompatInfo/trunk@214937 c90b9560-bf6c-de11-be94-00142212c4b1

--- docs/examples/Cli.php
+++ docs/examples/Cli.php
@@ -1,9 +1,16 @@
 <?php
+/**
+ * Get the Compatibility info from PHP CLI
+ *
+ * @version    $Id$
+ * @author     Davey Shafik <davey@php.net>
+ * @package    PHP_CompatInfo
+ * @access     public
+ * @ignore
+ */
 
 require_once 'PHP/CompatInfo/Cli.php';
 
 $cli = new PHP_CompatInfo_Cli();
-
 $cli->run();
-
 ?>
