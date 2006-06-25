0553f882a03cacbc2936f5a0292532d3714a3bf8
Laurent Laville <farell@php.net>
2006-06-25 15:53:27 +0000

    clean-up array for php 5 constants detection (old implementation from release less than 1.0.1)
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CompatInfo/trunk@215331 c90b9560-bf6c-de11-be94-00142212c4b1

--- CompatInfo/const_array.php
+++ CompatInfo/const_array.php
@@ -4,26 +4,6 @@
  * @global array $GLOBALS['_PHP_COMPATINFO_CONST']
  */
 $GLOBALS['_PHP_COMPATINFO_CONST'] = array (
-  'tokens' =>
-    array (
-    0 => 363,
-    1 => 362,
-    2 => 361,
-    3 => 359,
-    4 => 360,
-    5 => 345,
-    6 => 337,
-    7 => 344,
-    8 => 288,
-    9 => 343,
-    10 => 342,
-    11 => 341,
-    12 => 338,
-    13 => 336,
-    14 => 298,
-    15 => 352,
-    16 => 354,
-  ),
   '__FILE__' =>
   array (
     'init' => '4.0.0',
@@ -110,26 +90,4 @@ $GLOBALS['_PHP_COMPATINFO_CONST'] = array (
     'name' => 'implements',
   ),
 );
-
-$php5_only = array(
-                298 => 'T_CLONE',
-                360 => '__METHOD__',
-                343 => 'T_PRIVATE',
-                342 => 'T_PUBLIC',
-                341 => 'T_PROTECTED',
-                338 => 'T_THROW',
-                337 => 'T_CATCH',
-                336 => 'T_TRY',
-                352 => 'T_INTERFACE',
-                345 => 'T_ABSTRACT',
-                288 => 'T_INSTANCEOF',
-                344 => 'T_FINAL',
-                 );
-
-
-if (!defined('T_CLONE')) {
-    foreach ($php5_only as $i => $token) {
-        unset($GLOBALS['_PHP_COMPATINFO_CONST']['tokens'][$i]);
-    }
-}
 ?>
