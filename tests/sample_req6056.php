e59f724c4e74a994584236eef6599bdebcc45f5f
Laurent Laville <farell@php.net>
2006-06-27 22:21:37 +0000

    new test scripts to check php max version (request 6056)
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CompatInfo/trunk@215444 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ tests/sample_req6056.php
@@ -0,0 +1,12 @@
+<?php
+
+// PHP 5 <= 5.0.4
+$res = php_check_syntax('bug6581.php');
+
+$array1 = array('blue'  => 1, 'red'  => 2, 'green'  => 3);
+$array2 = array('green' => 5, 'blue' => 6, 'yellow' => 7);
+
+// PHP 5 >= 5.1.0RC1
+$diff = array_diff_key($array1, $array2);
+
+?>
