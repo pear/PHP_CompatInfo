8d1e354fc51f724efe2115d38e53d346a6114cbc
Laurent Laville <farell@php.net>
2006-08-27 16:32:26 +0000

    windows script user to run CLI version of PHP_CompatInfo
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CompatInfo/trunk@218942 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ scripts/compatinfo.bat
@@ -0,0 +1,22 @@
+@echo off
+
+rem CLI Script to Check Compatibility of chunk of PHP code
+
+rem PHP versions 4 and 5
+
+rem LICENSE: This source file is subject to version 3.0 of the PHP license
+rem that is available through the world-wide-web at the following URI:
+rem http://www.php.net/license/3_0.txt.  If you did not receive a copy of
+rem the PHP License and are unable to obtain it through the web, please
+rem send a note to license@php.net so we can mail you a copy immediately.
+
+rem @category   PHP
+rem @package    PHP_CompatInfo
+rem @author     Laurent Laville <pear@laurent-laville.org>
+rem @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+rem @version    CVS: $Id: compatinfo.bat,v 1.1 2006-08-27 16:32:26 farell Exp $
+rem @link       http://pear.php.net/package/PHP_CompatInfo
+rem @since      File available since Release 1.3.0
+
+@php_bin@ -d include_path="@php_dir@" -f @php_dir@/PHP/CompatInfo/pcicmd.php -- %1 %2 %3
+
