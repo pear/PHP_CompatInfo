5036092efef5f7d98c7e07c00cb5233127e98cb3
Laurent Laville <farell@php.net>
2007-02-10 15:18:06 +0000

    - upgrade license from PHP 3.0 to 3.01 (with agreement of Davey Shafik) - more phpdoc tags space identation
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CompatInfo/trunk@229485 c90b9560-bf6c-de11-be94-00142212c4b1

--- CompatInfo.php
+++ CompatInfo.php
@@ -4,9 +4,9 @@
  *
  * PHP versions 4 and 5
  *
- * LICENSE: This source file is subject to version 3.0 of the PHP license
+ * LICENSE: This source file is subject to version 3.01 of the PHP license
  * that is available through the world-wide-web at the following URI:
- * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
+ * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
  * the PHP License and are unable to obtain it through the web, please
  * send a note to license@php.net so we can mail you a copy immediately.
  *
@@ -14,7 +14,7 @@
  * @package    PHP_CompatInfo
  * @author     Davey Shafik <davey@php.net>
  * @author     Laurent Laville <pear@laurent-laville.org>
- * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
  * @version    CVS: $Id$
  * @link       http://pear.php.net/package/PHP_CompatInfo
  * @since      File available since Release 0.7.0
@@ -45,7 +45,7 @@ require_once 'PHP/CompatInfo/const_array.php';
  * @author     Davey Shafik <davey@php.net>
  * @author     Laurent Laville <pear@laurent-laville.org>
  * @copyright  Copyright 2003 Davey Shafik and Synaptic Media. All Rights Reserved.
- * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
  * @version    Release: @package_version@
  * @link       http://pear.php.net/package/PHP_CompatInfo
  * @since      Class available since Release 0.7.0
@@ -393,8 +393,8 @@ class PHP_CompatInfo
      * token_get_all() which is nicely
      * wrapped in PHP_CompatInfo::_tokenize
      *
-     * @param array $tokens Array of PHP Tokens
-     * @param boolean $debug Show Extra Output
+     * @param  array   $tokens Array of PHP Tokens
+     * @param  boolean $debug  Show Extra Output
      * @access private
      * @return array
      * @since  0.7.0
@@ -597,8 +597,8 @@ class PHP_CompatInfo
     /**
      * Token a file or string
      *
-     * @param string $input Filename or PHP code
-     * @param boolean $is_string Whether or note the input is a string
+     * @param  string  $input     Filename or PHP code
+     * @param  boolean $is_string Whether or note the input is a string
      * @access private
      * @return array|false
      * @since  0.7.0
@@ -620,7 +620,7 @@ class PHP_CompatInfo
      * Retrieve a listing of every file in $directory and
      * all subdirectories. Taken from PEAR_PackageFileManager_File
      *
-     * @param string $directory full path to the directory you want the list of
+     * @param  string $directory full path to the directory you want the list of
      * @access private
      * @return array list of files in a directory
      * @since  0.7.0
