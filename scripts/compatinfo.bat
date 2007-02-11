a0db4dd1d83475add2757b084d20767b4ee2ec9a
Laurent Laville <farell@php.net>
2007-02-11 16:20:50 +0000

    fix missing upgrade license from PHP 3.0 to 3.01
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CompatInfo/trunk@229559 c90b9560-bf6c-de11-be94-00142212c4b1

--- scripts/compatinfo.bat
+++ scripts/compatinfo.bat
@@ -4,17 +4,17 @@ rem CLI Script to Check Compatibility of chunk of PHP code
 
 rem PHP versions 4 and 5
 
-rem LICENSE: This source file is subject to version 3.0 of the PHP license
+rem LICENSE: This source file is subject to version 3.01 of the PHP license
 rem that is available through the world-wide-web at the following URI:
-rem http://www.php.net/license/3_0.txt.  If you did not receive a copy of
+rem http://www.php.net/license/3_01.txt.  If you did not receive a copy of
 rem the PHP License and are unable to obtain it through the web, please
 rem send a note to license@php.net so we can mail you a copy immediately.
 
 rem @category   PHP
 rem @package    PHP_CompatInfo
 rem @author     Laurent Laville <pear@laurent-laville.org>
-rem @license    http://www.php.net/license/3_0.txt  PHP License 3.0
-rem @version    CVS: $Id: compatinfo.bat,v 1.3 2006-09-24 09:18:52 farell Exp $
+rem @license    http://www.php.net/license/3_01.txt  PHP License 3.01
+rem @version    CVS: $Id: compatinfo.bat,v 1.4 2007-02-11 16:20:50 farell Exp $
 rem @link       http://pear.php.net/package/PHP_CompatInfo
 rem @since      File available since Release 1.3.0
 
