5a153f009bf7a6bd8a3ec0648d7ab99e3e22ff84
Laurent Laville <farell@php.net>
2007-11-03 17:47:45 +0000

    avoid phpdoc api generation
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CompatInfo/trunk@245515 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/sample_req6056.php
+++ tests/sample_req6056.php
@@ -1,10 +1,35 @@
 <?php
-// PHP 5 <= 5.0.4
-$res = php_check_syntax('bug6581.php');
+/**
+ * Request #6056 Add support for reporting max PHP version. Sample code.
+ *
+ * PHP versions 4 and 5
+ *
+ * @category PHP
+ * @package  PHP_CompatInfo
+ * @author   Laurent Laville <pear@laurent-laville.org>
+ * @license  http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version  CVS: $Id$
+ * @link     http://pear.php.net/bugs/bug.php?id=6056
+ * @ignore
+ */
 
-$array1 = array('blue'  => 1, 'red'  => 2, 'green'  => 3);
-$array2 = array('green' => 5, 'blue' => 6, 'yellow' => 7);
+class Request6056
+{
+    /**
+     * Sample code to check max php version
+     *
+     * @return void
+     */
+    function testMaxVersion()
+    {
+        // PHP 5 <= 5.0.4
+        $res = php_check_syntax('bug6581.php');
 
-// PHP 5 >= 5.1.0RC1
-$diff = array_diff_key($array1, $array2);
+        $array1 = array('blue'  => 1, 'red'  => 2, 'green'  => 3);
+        $array2 = array('green' => 5, 'blue' => 6, 'yellow' => 7);
+
+        // PHP 5 >= 5.1.0RC1
+        $diff = array_diff_key($array1, $array2);
+    }
+}
 ?>
