a4d9a96d6868095f404c24bfc820c964467f7715
Laurent Laville <farell@php.net>
2008-01-21 22:51:22 +0000

    resource to test extensions : std and pecl, while parsing directories
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CompatInfo/trunk@251104 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ tests/parseDir/extensions.php
@@ -0,0 +1,19 @@
+<?php
+xdebug_start_trace();
+
+function test_extensions()
+{
+    $image = imagecreate(320, 240);
+    imageantialias($image, true);
+    return $image;
+}
+
+print_r(apache_get_modules());
+
+if (!extension_loaded('sqlite')) {
+    $prefix = (PHP_SHLIB_SUFFIX === 'dll') ? 'php_' : '';
+    dl($prefix . 'sqlite.' . PHP_SHLIB_SUFFIX);
+}
+
+xdebug_stop_trace();
+?>
