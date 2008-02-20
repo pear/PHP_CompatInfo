12d92ee2262b0914ac2798eaef4d3307c606c435
Laurent Laville <farell@php.net>
2008-02-20 16:57:23 +0000

    add resource to test new API 1.7.0 option :  "ignore_functions_match"
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CompatInfo/trunk@253339 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ tests/parseFile/ignore_functions_match.php
@@ -0,0 +1,18 @@
+<?php
+function toFile($filename, $data)
+{
+    if (function_exists('file_put_contents')) {
+        file_put_contents($filename, $data);
+    } else {
+        $file = fopen($filename, 'wb');
+        fwrite($file, $data);
+        fclose($file);
+    }
+}
+
+if (function_exists('debug_backtrace')) {
+    $backtrace = debug_backtrace();
+} else {
+    $backtrace = false;
+}
+?>
