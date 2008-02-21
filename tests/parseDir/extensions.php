62e025f3709a8f60b6c73346800dac73859a9fe5
Laurent Laville <farell@php.net>
2008-02-21 12:34:24 +0000

    add a sqlite function to test suite with new "ignore_extensions_match" option ( API 1.7.0 )
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CompatInfo/trunk@253396 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/parseDir/extensions.php
+++ tests/parseDir/extensions.php
@@ -13,6 +13,7 @@ print_r(apache_get_modules());
 if (!extension_loaded('sqlite')) {
     $prefix = (PHP_SHLIB_SUFFIX === 'dll') ? 'php_' : '';
     dl($prefix . 'sqlite.' . PHP_SHLIB_SUFFIX);
+    echo 'SQLite version : ' . sqlite_libversion();
 }
 
 xdebug_stop_trace();
