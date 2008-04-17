8b70a8d00ad3d19665369ff3e41ec403705453b8
Laurent Laville <farell@php.net>
2008-04-17 20:58:51 +0000

    prepare stable release 1.7.0
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CompatInfo/trunk@257765 c90b9560-bf6c-de11-be94-00142212c4b1

--- package.php
+++ package.php
@@ -50,91 +50,73 @@ $p2->addRelease();
 $p2->addIgnoreToRelease('scripts/compatinfo.bat');
 $p2->addInstallAs('scripts/pci.php', 'pci');
 //$p2->addReplacement('scripts/pci.php', 'pear-config', '@php_bin@', 'php_bin');
-$p2->setReleaseVersion('1.7.0b4');
+$p2->setReleaseVersion('1.7.0');
 $p2->setAPIVersion('1.7.0');
-$p2->setReleaseStability('beta');
+$p2->setReleaseStability('stable');
 $p2->setAPIStability('stable');
-$p2->setNotes('LAST BETA before FINAL STABLE release.
-PLEASE TEST IT as much as possible !
+$p2->setNotes('After 1 alpha and 4 beta releases, I am pleased to announce the
+release of final stable version. This new API 1.7.0 introduce many new features.
+Here are a summary of all changes since API 1.6.0.
 
-TODO : Need to fix Regression Tests Suite
-
-Changes since version 1.7.0b3 (2008-03-28)
-
+All SAPI:
+=========
 * news
-- Use PEAR::File_Find package 1.3.0 or greater to manage "ignore_dirs" and
-  "ignore_files" easily (with ability to filter by regular expression).
+- add 3 options:
+  ignore_functions_match, ignore_extensions_match, ignore_constants_match.
+  ==> Request #12857
+- separate constants and tokens in results
+  ==> Request #13138
+
+* changes
+- full support of regular expressions for "ignore_dirs" and "ignore_files" options.
+- all reports display now the max version information when needed.
+
+* bugs
+- #13568 : User functions are not ignored
+- #13417 : Parser ignore class-method that are named as standard php functions
+- #13137 : Standard test suite failed under *nix
+
+CLI only:
+=========
+* news
+- Add new --output-level switch to customize (all) reports output.
+  ==> Request #13493
+- Add new --summarize switch to limit output to first line (summary)
+  when parsing directory.
+- Add new --version switch to print version number of PHP_CompatInfo package.
+- Add --filter-ext switch to filter file extension when parsing directory
+  ==> Request #13147
+- Add new column C to indicate a level of conditional code used (0: none,
+  1: function_exists() used, 2: extension_loaded() used, 4: defined() used)
+
+* changes
 - XML report structure change.
   Render is improve if PEAR::XML_Beautifier is available.
-- XML report may now be filtered with help of --output-level switch.
-- Values change for --output-level switch. Use now binary value (more easy to remember)
-  2->Extension, 4->Constant, 8->Tokens, with always 1->Path-File + Version
-  (Thanks to Stephan Wentz for his request)
-- Add new switch --summarize to limit output to first line (summary)
-  when parsing directory
-  (Thanks to Stephan Wentz for his request)
-
-* bugs
-- fix #13568 : User functions are not ignored
-
-Changes since version 1.7.0b2 (2008-03-24)
-
-* bugs
-- notice errors on XML report output when parsing a directory with CLI
-- add summary in XML report when parsing a directory with CLI
-
-* news
-- implement request #13493: customize CLI data output
-
-Changes since version 1.7.0b1 (2008-03-17)
-
 - pci.php file was rename to pci (without extension) to match unix command syntax
-- CLI may print (-V | --version) version number of PHP_CompatInfo package used
-
-* bugs
-- bug #13417 : Parser ignore class-method that are named as standard php functions
+- Text report is now really limited to 80 columns in all situation
+- All switchs that accept option file parameter allow now
+  to put line in comment with ; character (as in php.ini)
 
 * QA
-- dependencies related to CLI (Console_Table, Console_GetArgs, XML_Util)
+- dependencies Console_Table, Console_GetArgs, XML_Util
   are defined as required and no more optional. CLI is a standard feature and should
   not be considered as optional.
 
-Changes since version 1.7.0a1 (2008-02-21)
+---------------------------------------------------------------------------
+Changes since version 1.7.0b4 (2008-04-03)
 
-- fix CLI output render to 80 columns, on main table :
-  29 characters for File/Path column (1)
-   9 characters for Version column (2)
-  13 characters for Extensions column (3)
-  23 characters for Constants/Tokens column (4)
-- fix CLI output render to 80 columns, on additionnal tables :
-  25 characters for Option column (1)
-  51 characters for Value column (2)
-- On CLI, the XML report generation is now xml compliant with a root tag (pci)
-- On CLI, implement options:
-  ignore_functions_match  with -inm switch,
-  ignore_extensions_match with -iem switch,
-  ignore_constants_match  with -icm switch
-- On CLI options files (see -in, -ie, -ic, -inm, -iem, -icm)
-  allow to put line in comment with ; character (as in php.ini)
-
-Changes since stable version 1.6.1 (2008-02-16)
-
-* bugs
-- bug #13137 : Standard test suite failed under *nix
-
-* news
-- req #12857 : Add the option to locally mask exceptions
-  see new api 1.7.0 options:
-  ignore_functions_match, ignore_extensions_match, ignore_constants_match.
-- req #13138 : separate constants and tokens in results
-- req #13147 : CLI: add filter file extension option on parsing directory
+- parseString(), parseFile() and parseArray() handle now correctly the new
+  key-info "cond_code".
+- Add new column C to indicate a level of conditional code used.
+- CLI text report support version max information in main table results.
+- Regression Tests Suite was fixed.
 ');
 //$p2->setPearinstallerDep('1.5.4');
 //$p2->setPhpDep('4.3.10');
 //$p2->addPackageDepWithChannel('optional', 'PHPUnit', 'pear.phpunit.de', '3.2.0');
 //$p2->addPackageDepWithChannel('optional', 'XML_Util', 'pear.php.net', '1.1.4');
-$p2->addPackageDepWithChannel('required', 'File_Find', 'pear.php.net', '1.3.0');
-$p2->addPackageDepWithChannel('optional', 'XML_Beautifier', 'pear.php.net', '1.1');
+//$p2->addPackageDepWithChannel('required', 'File_Find', 'pear.php.net', '1.3.0');
+//$p2->addPackageDepWithChannel('optional', 'XML_Beautifier', 'pear.php.net', '1.1');
 
 if (isset($_GET['make']) ||
     (isset($_SERVER['argv']) && @$_SERVER['argv'][1] == 'make')) {
