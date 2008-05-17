6de142e26c8a490acb249ead240289f4b6f1d82a
Laurent Laville <farell@php.net>
2008-05-17 14:38:38 +0000

    example that may be run on all SAPI - but in CLI it will show a customized progress bar
    Explains given (inside) how to run it even on Windows platform
    
    
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CompatInfo/trunk@259873 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ docs/examples/parseDir_withCustomProgressBar.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Show a progress bar when parsing a directory from CLI
+ * Do not display any progress bar from other SAPI
+ *
+ * To run on Windows platform, do:
+ * (path to PHP cli)\php.exe -f (this script) -- -d (the dir you want to parse)
+ *
+ * PHP versions 4 and 5
+ *
+ * @category PHP
+ * @package  PHP_CompatInfo
+ * @author   Laurent Laville <pear@laurent-laville.org>
+ * @license  http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version  CVS: $Id$
+ * @link     http://pear.php.net/package/PHP_CompatInfo
+ * @ignore
+ */
+if (php_sapi_name() == 'cli') {
+    include_once 'PHP/CompatInfo/Cli.php';
+    /*
+      Display a progress bar like this one:
+
+     [)))))                                          ]    8% -  45/563 files
+
+     */
+    $pbar = array('formatString' => '[%bar%] %percent% - %fraction% files',
+                  'barfill' => ')',
+                  'prefill' => ' ',
+                  'options' => array('percent_precision' => 0));
+
+    $cli = new PHP_CompatInfo_Cli($pbar);
+    $cli->run();
+} else {
+    include_once 'PHP/CompatInfo.php';
+
+    $info = new PHP_CompatInfo($pbar);
+
+    $dir  = 'C:\php\pear\HTML_Progress2';
+    var_dump($info->parseDir($dir));
+}
+?>
