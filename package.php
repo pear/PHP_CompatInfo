fe3d9bef2f73bedf53013c5c18c3752daa1c4668
Laurent Laville <farell@php.net>
2008-05-18 07:26:11 +0000

    prepare release 1.8.0RC1
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CompatInfo/trunk@259896 c90b9560-bf6c-de11-be94-00142212c4b1

--- package.php
+++ package.php
@@ -50,66 +50,20 @@ $p2->addRelease();
 $p2->addIgnoreToRelease('scripts/compatinfo.bat');
 $p2->addInstallAs('scripts/pci.php', 'pci');
 //$p2->addReplacement('scripts/pci.php', 'pear-config', '@php_bin@', 'php_bin');
-$p2->setReleaseVersion('1.7.0');
-$p2->setAPIVersion('1.7.0');
-$p2->setReleaseStability('stable');
+$p2->setReleaseVersion('1.8.0RC1');
+$p2->setAPIVersion('1.8.0');
+$p2->setReleaseStability('beta');
 $p2->setAPIStability('stable');
-$p2->setNotes('After 1 alpha and 4 beta releases, I am pleased to announce the
-release of final stable version. This new API 1.7.0 introduce many new features.
-Here are a summary of all changes since API 1.6.0.
-
-All SAPI:
-=========
-* news
-- add 3 options:
-  ignore_functions_match, ignore_extensions_match, ignore_constants_match.
-  ==> Request #12857
-- separate constants and tokens in results
-  ==> Request #13138
-
-* changes
-- full support of regular expressions for "ignore_dirs" and "ignore_files" options.
-- all reports display now the max version information when needed.
+$p2->setNotes('Consider this version as a high bug fix release.
+Especially if you want to parse files/dirs with conditional code such as
+function_exists(), extension_loaded(), and defined().
 
 * bugs
-- #13568 : User functions are not ignored
-- #13417 : Parser ignore class-method that are named as standard php functions
-- #13137 : Standard test suite failed under *nix
+- #13873: Undefined Offset Error for function_exists detection
 
-CLI only:
-=========
 * news
-- Add new --output-level switch to customize (all) reports output.
-  ==> Request #13493
-- Add new --summarize switch to limit output to first line (summary)
-  when parsing directory.
-- Add new --version switch to print version number of PHP_CompatInfo package.
-- Add --filter-ext switch to filter file extension when parsing directory
-  ==> Request #13147
-- Add new column C to indicate a level of conditional code used (0: none,
-  1: function_exists() used, 2: extension_loaded() used, 4: defined() used)
-
-* changes
-- XML report structure change.
-  Render is improve if PEAR::XML_Beautifier is available.
-- pci.php file was rename to pci (without extension) to match unix command syntax
-- Text report is now really limited to 80 columns in all situation
-- All switchs that accept option file parameter allow now
-  to put line in comment with ; character (as in php.ini)
-
-* QA
-- dependencies Console_Table, Console_GetArgs, XML_Util
-  are defined as required and no more optional. CLI is a standard feature and should
-  not be considered as optional.
-
----------------------------------------------------------------------------
-Changes since version 1.7.0b4 (2008-04-03)
-
-- parseString(), parseFile() and parseArray() handle now correctly the new
-  key-info "cond_code".
-- Add new column C to indicate a level of conditional code used.
-- CLI text report support version max information in main table results.
-- Regression Tests Suite was fixed.
+- support of error_reporting constant E_RECOVERABLE_ERROR introduced with PHP 5.2.0
+- show a progress bar when parsing directories from CLI
 ');
 //$p2->setPearinstallerDep('1.5.4');
 //$p2->setPhpDep('4.3.10');
@@ -117,6 +71,7 @@ Changes since version 1.7.0b4 (2008-04-03)
 //$p2->addPackageDepWithChannel('optional', 'XML_Util', 'pear.php.net', '1.1.4');
 //$p2->addPackageDepWithChannel('required', 'File_Find', 'pear.php.net', '1.3.0');
 //$p2->addPackageDepWithChannel('optional', 'XML_Beautifier', 'pear.php.net', '1.1');
+$p2->addPackageDepWithChannel('optional', 'Console_ProgressBar', 'pear.php.net', '0.5.2beta');
 
 if (isset($_GET['make']) ||
     (isset($_SERVER['argv']) && @$_SERVER['argv'][1] == 'make')) {
