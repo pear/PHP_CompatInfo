61b8fdd6dd3edd993a8838e4f851a94b8c1291f4
Laurent Laville <farell@php.net>
2008-06-02 06:23:31 +0000

    API 1.8.0 implement MVC design pattern. Here is the "Null" renderer that consumes output results. Usefull when running in batch mode, and do not want to display each result
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CompatInfo/trunk@260598 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ CompatInfo/Renderer/Null.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Null renderer for PHP_CompatInfo component.
+ *
+ * PHP versions 4 and 5
+ *
+ * LICENSE: This source file is subject to version 3.01 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category PHP
+ * @package  PHP_CompatInfo
+ * @author   Laurent Laville <pear@laurent-laville.org>
+ * @license  http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version  CVS: $Id$
+ * @link     http://pear.php.net/package/PHP_CompatInfo
+ * @since    File available since Release 1.8.0b2
+ */
+
+/**
+ * The PHP_CompatInfo_Renderer_Null class is a concrete implementation
+ * of PHP_CompatInfo_Renderer abstract class. It simply consumes output events.
+ *
+ * @category PHP
+ * @package  PHP_CompatInfo
+ * @author   Laurent Laville <pear@laurent-laville.org>
+ * @license  http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version  Release: @package_version@
+ * @link     http://pear.php.net/package/PHP_CompatInfo
+ * @since    Class available since Release 1.8.0b2
+ */
+class PHP_CompatInfo_Renderer_Null extends PHP_CompatInfo_Renderer
+{
+    /**
+     * Null Renderer Class constructor (ZE1) for PHP4
+     *
+     * @param object &$parser Instance of the parser (model of MVC pattern)
+     * @param array  $conf    A hash containing any additional configuration
+     *
+     * @access public
+     * @since  version 1.8.0b2 (2008-06-03)
+     */
+    function PHP_CompatInfo_Renderer_Null(&$parser, $conf)
+    {
+        $this->_construct($parser, $conf);
+    }
+
+    /**
+     * Null Renderer Class constructor (ZE2) for PHP5+
+     *
+     * @param object &$parser Instance of the parser (model of MVC pattern)
+     * @param array  $conf    A hash containing any additional configuration
+     *
+     * @access public
+     * @since  version 1.8.0b2 (2008-06-03)
+     */
+    function __construct(&$parser, $conf)
+    {
+    }
+
+    /**
+     * Consumes output events
+     *
+     * @param object &$auditEvent Instance of Event_Dispatcher
+     *
+     * @access public
+     * @return void
+     * @since  version 1.8.0b2 (2008-06-03)
+     */
+    function update(&$auditEvent)
+    {
+    }
+}
+?>
