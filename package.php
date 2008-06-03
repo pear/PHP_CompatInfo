c92e016667b2f2dcb1e82796163534d6848f9d25
Laurent Laville <farell@php.net>
2008-06-03 20:26:57 +0000

    upcoming release 1.8.0b2
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CompatInfo/trunk@260697 c90b9560-bf6c-de11-be94-00142212c4b1

--- package.php
+++ package.php
@@ -35,7 +35,7 @@ $options = array('filelistgenerator' => 'cvs',
     'simpleoutput' => true,
     'clearcontents' => false,
     'changelogoldtonew' => false,
-    'ignore' => array(__FILE__,
+    'ignore' => array(__FILE__, 'tests/',
         'funclist.txt', 'updateVersionInfo.php', 'version.xml')
     );
 
@@ -50,28 +50,53 @@ $p2->addRelease();
 $p2->addIgnoreToRelease('scripts/compatinfo.bat');
 $p2->addInstallAs('scripts/pci.php', 'pci');
 //$p2->addReplacement('scripts/pci.php', 'pear-config', '@php_bin@', 'php_bin');
-$p2->setReleaseVersion('1.8.0b1');
+$p2->setReleaseVersion('1.8.0b2');
 $p2->setAPIVersion('1.8.0');
 $p2->setReleaseStability('beta');
 $p2->setAPIStability('stable');
-$p2->setNotes('Consider this version as a high bug fix release.
-Especially if you want to parse files/dirs with conditional code such as
-function_exists(), extension_loaded(), and defined().
-
-* bugs
-- #13873: Undefined Offset Error for function_exists detection
+$p2->setNotes('CREDITS
+Thanks to John Parise and Ken Guest, for their agreement to reuse code and idea
+from their packages. Idea about connecting a driver (see PEAR_Size) and
+how to build/check an instance of a custom renderer (see Log) if class
+is available loaded somewhere.
 
 * news
-- support of error_reporting constant E_RECOVERABLE_ERROR introduced with PHP 5.2.0
-- show a progress bar when parsing directories from CLI
+API 1.8.0 since beta 2 was almost full rewrite following the MVC design pattern.
+This version, that DO NOT break Backward Compatibility, introduces at least two big
+features :
+1. Event-Driven capability (see PEAR::Event_Dispatcher),
+2. Customizable and extendable Renderers
+
+Do not forget to have a look on 3 news examples pci180_parse* that demonstrates
+the new API.
+
+Beta 2 include 3 renderers :
+- "Null" that consumes all output. Usefull for batch mode
+- "Array" the default. That dump results as a PHP array (like previous versions)
+  but allow also to print improved array with help of PEAR::Var_Dump if available.
+- "Xml" that produces XML results with PEAR::XML_Util and improve its render
+  with PEAR::XML_Beautifier if available.
+
+  WARNING: if you use benefit of XML_Beautifier with the Xml renderer and see
+            the XML prolog/declaration be removed, DO NOT add a new bug report
+            it is the old Bug #5450 !
+
+Beta 3 that will come shortly will include 2 new more renderers :
+- Html, for web page content
+- Text, for command line interface (that will come back again live)
+
+* QA
+- Do no search Unit tests in this release, it is not yet ready, so I have removed
+  them until beta3
 ');
 //$p2->setPearinstallerDep('1.5.4');
 //$p2->setPhpDep('4.3.10');
+$p2->addPackageDepWithChannel('required', 'Event_Dispatcher', 'pear.php.net', '1.0.0');
 //$p2->addPackageDepWithChannel('optional', 'PHPUnit', 'pear.phpunit.de', '3.2.0');
 //$p2->addPackageDepWithChannel('optional', 'XML_Util', 'pear.php.net', '1.1.4');
 //$p2->addPackageDepWithChannel('required', 'File_Find', 'pear.php.net', '1.3.0');
 //$p2->addPackageDepWithChannel('optional', 'XML_Beautifier', 'pear.php.net', '1.1');
-$p2->addPackageDepWithChannel('optional', 'Console_ProgressBar', 'pear.php.net', '0.5.2beta');
+//$p2->addPackageDepWithChannel('optional', 'Console_ProgressBar', 'pear.php.net', '0.5.2beta');
 
 if (isset($_GET['make']) ||
     (isset($_SERVER['argv']) && @$_SERVER['argv'][1] == 'make')) {
