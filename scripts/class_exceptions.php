1d3d4ab7604dba5893a04400ffea4f6ae24ab7c8
Laurent Laville <farell@php.net>
2008-11-23 15:18:19 +0000

    base of components to build PHP extensions support list (classes and constants)
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CompatInfo/trunk@269556 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ scripts/class_exceptions.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Predefined Classes official version known for PHP_CompatInfo 1.9.0a1 or better
+ *
+ * PHP versions 5
+ *
+ * @category PHP
+ * @package  PHP_CompatInfo
+ * @author   Davey Shafik <davey@php.net>
+ * @author   Laurent Laville <pear@laurent-laville.org>
+ * @license  http://www.opensource.org/licenses/bsd-license.php  BSD
+ * @version  CVS: $Id$
+ * @link     http://pear.php.net/package/PHP_CompatInfo
+ * @ignore
+ */
+
+$class_exceptions = array (
+  'standard' => array(
+    'Exception' =>
+    array (
+      'init' => '5.0.0',
+    ),
+    'php_user_filter' =>
+    array (
+      'init' => '5.0.0',
+    ),
+    'stdClass' =>
+    array (
+      'init' => '4.0.0',
+    ),
+  ),
+);
+?>
---
+++ scripts/constant_exceptions.php
@@ -0,0 +1,233 @@
+<?php
+/**
+ * Constants official version known for PHP_CompatInfo 1.9.0a1 or better
+ *
+ * PHP versions 5
+ *
+ * @category PHP
+ * @package  PHP_CompatInfo
+ * @author   Davey Shafik <davey@php.net>
+ * @author   Laurent Laville <pear@laurent-laville.org>
+ * @license  http://www.opensource.org/licenses/bsd-license.php  BSD
+ * @version  CVS: $Id$
+ * @link     http://pear.php.net/package/PHP_CompatInfo
+ * @ignore
+ */
+
+$constant_exceptions = array (
+  'date' => array(
+    'DATE_ATOM' =>
+    array (
+      'init' => '5.1.1',
+      'name' => 'DATE_ATOM',
+    ),
+    'DATE_COOKIE' =>
+    array (
+      'init' => '5.1.1',
+      'name' => 'DATE_COOKIE',
+    ),
+    'DATE_ISO8601' =>
+    array (
+      'init' => '5.1.1',
+      'name' => 'DATE_ISO8601',
+    ),
+    'DATE_RFC822' =>
+    array (
+      'init' => '5.1.1',
+      'name' => 'DATE_RFC822',
+    ),
+    'DATE_RFC850' =>
+    array (
+      'init' => '5.1.1',
+      'name' => 'DATE_RFC850',
+    ),
+    'DATE_RFC1036' =>
+    array (
+      'init' => '5.1.1',
+      'name' => 'DATE_RFC1036',
+    ),
+    'DATE_RFC1123' =>
+    array (
+      'init' => '5.1.1',
+      'name' => 'DATE_RFC1123',
+    ),
+    'DATE_RFC2822' =>
+    array (
+      'init' => '5.1.1',
+      'name' => 'DATE_RFC2822',
+    ),
+    'DATE_RFC3339' =>
+    array (
+      'init' => '5.1.3',
+      'name' => 'DATE_RFC3339',
+    ),
+    'DATE_RSS' =>
+    array (
+      'init' => '5.1.1',
+      'name' => 'DATE_RSS',
+    ),
+    'DATE_W3C' =>
+    array (
+      'init' => '5.1.1',
+      'name' => 'DATE_W3C',
+    ),
+  ),
+  'standard' => array(
+    '__FUNCTION__' =>
+    array (
+      'init' => '4.3.0',
+      'name' => '__FUNCTION__',
+    ),
+    '__CLASS__' =>
+    array (
+      'init' => '4.3.0',
+      'name' => '__CLASS__',
+    ),
+    '__METHOD__' =>
+    array (
+      'name' => '__METHOD__',
+      'init' => '5.0.0',
+    ),
+    'ABSTRACT' =>
+    array (
+      'init' => '5.0.0',
+      'name' => 'abstract',
+    ),
+    'CATCH' =>
+    array (
+      'init' => '5.0.0',
+      'name' => 'catch',
+    ),
+    'FINAL' =>
+    array (
+      'init' => '5.0.0',
+      'name' => 'final',
+    ),
+    'INSTANCEOF' =>
+    array (
+      'init' => '5.0.0',
+      'name' => 'instanceof',
+    ),
+    'PRIVATE' =>
+    array (
+      'init' => '5.0.0',
+      'name' => 'private',
+    ),
+    'PROTECTED' =>
+    array (
+      'init' => '5.0.0',
+      'name' => 'protected',
+    ),
+    'PUBLIC' =>
+    array (
+      'init' => '5.0.0',
+      'name' => 'public',
+    ),
+    'THROW' =>
+    array (
+      'init' => '5.0.0',
+      'name' => 'throw',
+    ),
+    'TRY' =>
+    array (
+      'init' => '5.0.0',
+      'name' => 'try',
+    ),
+    'CLONE' =>
+    array (
+      'init' => '5.0.0',
+      'name' => 'clone',
+    ),
+    'INTERFACE' =>
+    array (
+      'init' => '5.0.0',
+      'name' => 'interface',
+    ),
+    'IMPLEMENTS' =>
+    array (
+      'init' => '5.0.0',
+      'name' => 'implements',
+    ),
+    'PHP_EOL' =>
+    array (
+      'init' => '4.3.10',
+      'name' => 'PHP_EOL',
+    ),
+    'DIRECTORY_SEPARATOR' =>
+    array (
+      'init' => '4.0.6',
+      'name' => 'DIRECTORY_SEPARATOR',
+    ),
+    'E_STRICT' =>
+    array (
+      'init' => '5.0.0',
+      'name' => 'E_STRICT',
+    ),
+    'E_RECOVERABLE_ERROR' =>
+    array (
+      'init' => '5.2.0',
+      'name' => 'E_RECOVERABLE_ERROR',
+    ),
+    'PATH_SEPARATOR' =>
+    array (
+      'init' => '4.3.0',
+      'name' => 'PATH_SEPARATOR',
+    ),
+    'STDIN' =>
+    array (
+      'init' => '4.3.0',
+      'name' => 'STDIN',
+    ),
+    'STDOUT' =>
+    array (
+      'init' => '4.3.0',
+      'name' => 'STDOUT',
+    ),
+    'STDERR' =>
+    array (
+      'init' => '4.3.0',
+      'name' => 'STDERR',
+    ),
+    'UPLOAD_ERR_OK' =>
+    array (
+      'init' => '4.3.0',
+      'name' => 'UPLOAD_ERR_OK',
+    ),
+    'UPLOAD_ERR_INI_SIZE' =>
+    array (
+      'init' => '4.3.0',
+      'name' => 'UPLOAD_ERR_INI_SIZE',
+    ),
+    'UPLOAD_ERR_FORM_SIZE' =>
+    array (
+      'init' => '4.3.0',
+      'name' => 'UPLOAD_ERR_FORM_SIZE',
+    ),
+    'UPLOAD_ERR_PARTIAL' =>
+    array (
+      'init' => '4.3.0',
+      'name' => 'UPLOAD_ERR_PARTIAL',
+    ),
+    'UPLOAD_ERR_NO_FILE' =>
+    array (
+      'init' => '4.3.0',
+      'name' => 'UPLOAD_ERR_NO_FILE',
+    ),
+    'UPLOAD_ERR_NO_TMP_DIR' =>
+    array (
+      'init' => '4.3.10',
+      'name' => 'UPLOAD_ERR_NO_TMP_DIR',
+    ),
+    'UPLOAD_ERR_CANT_WRITE' =>
+    array (
+      'init' => '5.1.0',
+      'name' => 'UPLOAD_ERR_CANT_WRITE',
+    ),
+    'UPLOAD_ERR_EXTENSION' =>
+    array (
+      'init' => '5.2.0',
+      'name' => 'UPLOAD_ERR_EXTENSION',
+    ),
+  ),
+);
+?>
