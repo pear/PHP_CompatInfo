d2a38215890b396d64311d5e40fd9369eb2444f6
Laurent Laville <farell@php.net>
2008-12-13 16:46:11 +0000

    (initial commit) 1.9.0b2 dictionaries for extension: json
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CompatInfo/trunk@271173 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ CompatInfo/json_func_array.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * json extension Function dictionary for PHP_CompatInfo 1.9.0b2 or better
+ *
+ * PHP versions 4 and 5
+ *
+ * @category PHP
+ * @package  PHP_CompatInfo
+ * @author   Davey Shafik <davey@php.net>
+ * @author   Laurent Laville <pear@laurent-laville.org>
+ * @license  http://www.opensource.org/licenses/bsd-license.php  BSD
+ * @version  CVS: $Id$
+ * @link     http://pear.php.net/package/PHP_CompatInfo
+ * @since    version 1.9.0b2 (2008-12-19)
+ */
+
+$GLOBALS['_PHP_COMPATINFO_FUNC_JSON'] = array (
+  'json_decode' =>
+  array (
+    'init' => '1.2.0',
+    'end' => '1.2.1',
+    'ext' => 'json',
+    'pecl' => true,
+  ),
+  'json_encode' =>
+  array (
+    'init' => '1.2.0',
+    'end' => '1.2.1',
+    'ext' => 'json',
+    'pecl' => true,
+  ),
+);
+?>
