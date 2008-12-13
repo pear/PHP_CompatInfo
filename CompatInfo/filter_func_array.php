31bc1086948d2d721face464decff53a0c4fc12e
Laurent Laville <farell@php.net>
2008-12-13 16:36:59 +0000

    (initial commit) 1.9.0b2 dictionaries for extension: filter
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CompatInfo/trunk@271166 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ CompatInfo/filter_func_array.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * filter extension Function dictionary for PHP_CompatInfo 1.9.0b2 or better
+ *
+ * PHP versions 4 and 5
+ *
+ * @category PHP
+ * @package  PHP_CompatInfo
+ * @author   Davey Shafik <davey@php.net>
+ * @author   Laurent Laville <pear@laurent-laville.org>
+ * @license  http://www.opensource.org/licenses/bsd-license.php  BSD
+ * @version  CVS: $Id$
+ * @link     http://pear.php.net/package/PHP_CompatInfo
+ * @since    version 1.9.0b2 (2008-12-19)
+ */
+
+$GLOBALS['_PHP_COMPATINFO_FUNC_FILTER'] = array (
+  'filter_has_var' =>
+  array (
+    'init' => '5.2.0',
+    'ext' => 'filter',
+    'pecl' => false,
+  ),
+  'filter_id' =>
+  array (
+    'init' => '5.2.0',
+    'ext' => 'filter',
+    'pecl' => false,
+  ),
+  'filter_input' =>
+  array (
+    'init' => '5.2.0',
+    'ext' => 'filter',
+    'pecl' => false,
+  ),
+  'filter_input_array' =>
+  array (
+    'init' => '5.2.0',
+    'ext' => 'filter',
+    'pecl' => false,
+  ),
+  'filter_list' =>
+  array (
+    'init' => '5.2.0',
+    'ext' => 'filter',
+    'pecl' => false,
+  ),
+  'filter_var' =>
+  array (
+    'init' => '5.2.0',
+    'ext' => 'filter',
+    'pecl' => false,
+  ),
+  'filter_var_array' =>
+  array (
+    'init' => '5.2.0',
+    'ext' => 'filter',
+    'pecl' => false,
+  ),
+  'input_filters_list' =>
+  array (
+    'init' => '0.10.0',
+    'end' => '0.9.4',
+    'ext' => 'filter',
+    'pecl' => true,
+  ),
+  'input_get' =>
+  array (
+    'init' => '0.10.0',
+    'end' => '0.9.4',
+    'ext' => 'filter',
+    'pecl' => true,
+  ),
+  'input_get_args' =>
+  array (
+    'init' => '0.10.0',
+    'end' => '0.10.0',
+    'ext' => 'filter',
+    'pecl' => true,
+  ),
+  'input_has_variable' =>
+  array (
+    'init' => '0.10.0',
+    'end' => '0.9.4',
+    'ext' => 'filter',
+    'pecl' => true,
+  ),
+  'input_name_to_filter' =>
+  array (
+    'init' => '0.10.0',
+    'end' => '0.9.4',
+    'ext' => 'filter',
+    'pecl' => true,
+  ),
+);
+?>
