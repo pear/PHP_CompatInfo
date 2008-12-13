860a33243600cd12edc2b222dc3ab61995bb1c91
Laurent Laville <farell@php.net>
2008-12-13 16:35:27 +0000

    (initial commit) 1.9.0b2 dictionaries for extension: dom
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CompatInfo/trunk@271165 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ CompatInfo/dom_func_array.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * dom extension Function dictionary for PHP_CompatInfo 1.9.0b2 or better
+ *
+ * PHP versions 4 and 5
+ *
+ * @category PHP
+ * @package  PHP_CompatInfo
+ * @author   Davey Shafik <davey@php.net>
+ * @author   Laurent Laville <pear@laurent-laville.org>
+ * @license  http://www.opensource.org/licenses/bsd-license.php  BSD
+ * @version  CVS: $Id$
+ * @link     http://pear.php.net/package/PHP_CompatInfo
+ * @since    version 1.9.0b2 (2008-12-19)
+ */
+
+$GLOBALS['_PHP_COMPATINFO_FUNC_DOM'] = array (
+  'dom_import_simplexml' =>
+  array (
+    'init' => '5.0.0',
+    'ext' => 'dom',
+    'pecl' => false,
+  ),
+);
+?>
