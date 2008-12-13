079a62702cea9679cb83c2fdb59ae72296d6d532
Laurent Laville <farell@php.net>
2008-12-13 16:41:53 +0000

    (initial commit) 1.9.0b2 dictionaries for extension: hash
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CompatInfo/trunk@271170 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ CompatInfo/hash_func_array.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * hash extension Function dictionary for PHP_CompatInfo 1.9.0b2 or better
+ *
+ * PHP versions 4 and 5
+ *
+ * @category PHP
+ * @package  PHP_CompatInfo
+ * @author   Davey Shafik <davey@php.net>
+ * @author   Laurent Laville <pear@laurent-laville.org>
+ * @license  http://www.opensource.org/licenses/bsd-license.php  BSD
+ * @version  CVS: $Id$
+ * @link     http://pear.php.net/package/PHP_CompatInfo
+ * @since    version 1.9.0b2 (2008-12-19)
+ */
+
+$GLOBALS['_PHP_COMPATINFO_FUNC_HASH'] = array (
+  'hash' =>
+  array (
+    'init' => '1.1',
+    'end' => '1.5',
+    'ext' => 'hash',
+    'pecl' => true,
+  ),
+  'hash_algos' =>
+  array (
+    'init' => '1.1',
+    'end' => '1.5',
+    'ext' => 'hash',
+    'pecl' => true,
+  ),
+  'hash_file' =>
+  array (
+    'init' => '1.1',
+    'end' => '1.5',
+    'ext' => 'hash',
+    'pecl' => true,
+  ),
+  'hash_final' =>
+  array (
+    'init' => '1.1',
+    'end' => '1.5',
+    'ext' => 'hash',
+    'pecl' => true,
+  ),
+  'hash_hmac' =>
+  array (
+    'init' => '1.1',
+    'end' => '1.5',
+    'ext' => 'hash',
+    'pecl' => true,
+  ),
+  'hash_hmac_file' =>
+  array (
+    'init' => '1.1',
+    'end' => '1.5',
+    'ext' => 'hash',
+    'pecl' => true,
+  ),
+  'hash_init' =>
+  array (
+    'init' => '1.1',
+    'end' => '1.5',
+    'ext' => 'hash',
+    'pecl' => true,
+  ),
+  'hash_update' =>
+  array (
+    'init' => '1.1',
+    'end' => '1.5',
+    'ext' => 'hash',
+    'pecl' => true,
+  ),
+  'hash_update_file' =>
+  array (
+    'init' => '1.1',
+    'end' => '1.5',
+    'ext' => 'hash',
+    'pecl' => true,
+  ),
+  'hash_update_stream' =>
+  array (
+    'init' => '1.1',
+    'end' => '1.5',
+    'ext' => 'hash',
+    'pecl' => true,
+  ),
+  'md5' =>
+  array (
+    'init' => '1.1',
+    'end' => '1.3',
+    'ext' => 'hash',
+    'pecl' => true,
+  ),
+  'md5_file' =>
+  array (
+    'init' => '1.1',
+    'end' => '1.3',
+    'ext' => 'hash',
+    'pecl' => true,
+  ),
+  'sha1' =>
+  array (
+    'init' => '1.1',
+    'end' => '1.3',
+    'ext' => 'hash',
+    'pecl' => true,
+  ),
+  'sha1_file' =>
+  array (
+    'init' => '1.1',
+    'end' => '1.3',
+    'ext' => 'hash',
+    'pecl' => true,
+  ),
+);
+?>
