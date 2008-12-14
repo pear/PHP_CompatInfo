14fe0d99a36aaae0f17329f5fd740450ca1c0af3
Laurent Laville <farell@php.net>
2008-12-14 17:23:24 +0000

    (initial commit) 1.9.0b2 dictionaries for extension: xmlwriter
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CompatInfo/trunk@271255 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ CompatInfo/xmlwriter_func_array.php
@@ -0,0 +1,313 @@
+<?php
+/**
+ * xmlwriter extension Function dictionary for PHP_CompatInfo 1.9.0b2 or better
+ *
+ * PHP versions 4 and 5
+ *
+ * @category PHP
+ * @package  PHP_CompatInfo
+ * @author   Davey Shafik <davey@php.net>
+ * @author   Laurent Laville <pear@laurent-laville.org>
+ * @license  http://www.opensource.org/licenses/bsd-license.php  BSD
+ * @version  CVS: $Id$
+ * @link     http://pear.php.net/package/PHP_CompatInfo
+ * @since    version 1.9.0b2 (2008-12-19)
+ */
+
+$GLOBALS['_PHP_COMPATINFO_FUNC_XMLWRITER'] = array (
+  'xmlwriter_end_attribute' =>
+  array (
+    'init' => '0.1',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+  'xmlwriter_end_cdata' =>
+  array (
+    'init' => '0.1',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+  'xmlwriter_end_comment' =>
+  array (
+    'init' => '1.0',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+  'xmlwriter_end_document' =>
+  array (
+    'init' => '0.1',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+  'xmlwriter_end_dtd' =>
+  array (
+    'init' => '0.1',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+  'xmlwriter_end_dtd_attlist' =>
+  array (
+    'init' => '0.1',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+  'xmlwriter_end_dtd_element' =>
+  array (
+    'init' => '0.1',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+  'xmlwriter_end_dtd_entity' =>
+  array (
+    'init' => '0.1',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+  'xmlwriter_end_element' =>
+  array (
+    'init' => '0.1',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+  'xmlwriter_end_pi' =>
+  array (
+    'init' => '0.1',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+  'xmlwriter_flush' =>
+  array (
+    'init' => '1.0',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+  'xmlwriter_full_end_element' =>
+  array (
+    'init' => '2.0.4',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+  'xmlwriter_open_memory' =>
+  array (
+    'init' => '0.1',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+  'xmlwriter_open_uri' =>
+  array (
+    'init' => '0.1',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+  'xmlwriter_output_memory' =>
+  array (
+    'init' => '0.1',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+  'xmlwriter_set_indent' =>
+  array (
+    'init' => '0.1',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+  'xmlwriter_set_indent_string' =>
+  array (
+    'init' => '0.1',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+  'xmlwriter_start_attribute' =>
+  array (
+    'init' => '0.1',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+  'xmlwriter_start_attribute_ns' =>
+  array (
+    'init' => '0.1',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+  'xmlwriter_start_cdata' =>
+  array (
+    'init' => '0.1',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+  'xmlwriter_start_comment' =>
+  array (
+    'init' => '1.0',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+  'xmlwriter_start_document' =>
+  array (
+    'init' => '0.1',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+  'xmlwriter_start_dtd' =>
+  array (
+    'init' => '0.1',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+  'xmlwriter_start_dtd_attlist' =>
+  array (
+    'init' => '0.1',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+  'xmlwriter_start_dtd_element' =>
+  array (
+    'init' => '0.1',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+  'xmlwriter_start_dtd_entity' =>
+  array (
+    'init' => '0.1',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+  'xmlwriter_start_element' =>
+  array (
+    'init' => '0.1',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+  'xmlwriter_start_element_ns' =>
+  array (
+    'init' => '0.1',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+  'xmlwriter_start_pi' =>
+  array (
+    'init' => '0.1',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+  'xmlwriter_text' =>
+  array (
+    'init' => '0.1',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+  'xmlwriter_write_attribute' =>
+  array (
+    'init' => '0.1',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+  'xmlwriter_write_attribute_ns' =>
+  array (
+    'init' => '0.1',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+  'xmlwriter_write_cdata' =>
+  array (
+    'init' => '0.1',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+  'xmlwriter_write_comment' =>
+  array (
+    'init' => '0.1',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+  'xmlwriter_write_dtd' =>
+  array (
+    'init' => '0.1',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+  'xmlwriter_write_dtd_attlist' =>
+  array (
+    'init' => '0.1',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+  'xmlwriter_write_dtd_element' =>
+  array (
+    'init' => '0.1',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+  'xmlwriter_write_dtd_entity' =>
+  array (
+    'init' => '0.1',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+  'xmlwriter_write_element' =>
+  array (
+    'init' => '0.1',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+  'xmlwriter_write_element_ns' =>
+  array (
+    'init' => '0.1',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+  'xmlwriter_write_pi' =>
+  array (
+    'init' => '0.1',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+  'xmlwriter_write_raw' =>
+  array (
+    'init' => '2.0.4',
+    'end' => '2.0.4',
+    'ext' => 'xmlwriter',
+    'pecl' => true,
+  ),
+);
+?>
