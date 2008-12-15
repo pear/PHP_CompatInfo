9d4174126049ac6cf8ad7c2b694a706d4116343f
Laurent Laville <farell@php.net>
2008-12-15 19:11:19 +0000

    (initial commit) 1.9.0b2 dictionaries for extension: date
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CompatInfo/trunk@271293 c90b9560-bf6c-de11-be94-00142212c4b1

--- CompatInfo/date_const_array.php
+++ CompatInfo/date_const_array.php
@@ -12,7 +12,6 @@
  * @version  CVS: $Id$
  * @link     http://pear.php.net/package/PHP_CompatInfo
  * @since    version 1.9.0a1 (2008-11-23)
- * @ignore
  */
 
 $GLOBALS['_PHP_COMPATINFO_CONST_DATE'] = array (
---
+++ CompatInfo/date_func_array.php
@@ -0,0 +1,223 @@
+<?php
+/**
+ * date extension Function dictionary for PHP_CompatInfo 1.9.0b2 or better
+ *
+ * PHP versions 4 and 5
+ *
+ * @category PHP
+ * @package  PHP_CompatInfo
+ * @author   Davey Shafik <davey@php.net>
+ * @author   Laurent Laville <pear@laurent-laville.org>
+ * @license  http://www.opensource.org/licenses/bsd-license.php  BSD
+ * @version  CVS: $Id$
+ * @link     http://pear.php.net/package/PHP_CompatInfo
+ * @since    version 1.9.0b2 (2008-12-19)
+ */
+
+$GLOBALS['_PHP_COMPATINFO_FUNC_DATE'] = array (
+  'checkdate' =>
+  array (
+    'init' => '4.0.0',
+    'ext' => 'date',
+    'pecl' => false,
+  ),
+  'date' =>
+  array (
+    'init' => '4.0.0',
+    'ext' => 'date',
+    'pecl' => false,
+  ),
+  'date_create' =>
+  array (
+    'init' => '5.1.0',
+    'ext' => 'date',
+    'pecl' => false,
+  ),
+  'date_date_set' =>
+  array (
+    'init' => '5.1.0',
+    'ext' => 'date',
+    'pecl' => false,
+  ),
+  'date_default_timezone_get' =>
+  array (
+    'init' => '5.1.0',
+    'ext' => 'date',
+    'pecl' => false,
+  ),
+  'date_default_timezone_set' =>
+  array (
+    'init' => '5.1.0',
+    'ext' => 'date',
+    'pecl' => false,
+  ),
+  'date_format' =>
+  array (
+    'init' => '5.1.0',
+    'ext' => 'date',
+    'pecl' => false,
+  ),
+  'date_isodate_set' =>
+  array (
+    'init' => '5.1.0',
+    'ext' => 'date',
+    'pecl' => false,
+  ),
+  'date_modify' =>
+  array (
+    'init' => '5.1.0',
+    'ext' => 'date',
+    'pecl' => false,
+  ),
+  'date_offset_get' =>
+  array (
+    'init' => '5.1.0',
+    'ext' => 'date',
+    'pecl' => false,
+  ),
+  'date_parse' =>
+  array (
+    'init' => '5.1.3',
+    'ext' => 'date',
+    'pecl' => false,
+  ),
+  'date_sun_info' =>
+  array (
+    'init' => '5.1.2',
+    'ext' => 'date',
+    'pecl' => false,
+  ),
+  'date_sunrise' =>
+  array (
+    'init' => '5.0.0',
+    'ext' => 'date',
+    'pecl' => false,
+  ),
+  'date_sunset' =>
+  array (
+    'init' => '5.0.0',
+    'ext' => 'date',
+    'pecl' => false,
+  ),
+  'date_time_set' =>
+  array (
+    'init' => '5.1.0',
+    'ext' => 'date',
+    'pecl' => false,
+  ),
+  'date_timezone_get' =>
+  array (
+    'init' => '5.1.0',
+    'ext' => 'date',
+    'pecl' => false,
+  ),
+  'date_timezone_set' =>
+  array (
+    'init' => '5.1.0',
+    'ext' => 'date',
+    'pecl' => false,
+  ),
+  'getdate' =>
+  array (
+    'init' => '4.0.0',
+    'ext' => 'date',
+    'pecl' => false,
+  ),
+  'gmdate' =>
+  array (
+    'init' => '4.0.0',
+    'ext' => 'date',
+    'pecl' => false,
+  ),
+  'gmmktime' =>
+  array (
+    'init' => '4.0.0',
+    'ext' => 'date',
+    'pecl' => false,
+  ),
+  'gmstrftime' =>
+  array (
+    'init' => '4.0.0',
+    'ext' => 'date',
+    'pecl' => false,
+  ),
+  'idate' =>
+  array (
+    'init' => '5.0.0',
+    'ext' => 'date',
+    'pecl' => false,
+  ),
+  'localtime' =>
+  array (
+    'init' => '4.0.0',
+    'ext' => 'date',
+    'pecl' => false,
+  ),
+  'mktime' =>
+  array (
+    'init' => '4.0.0',
+    'ext' => 'date',
+    'pecl' => false,
+  ),
+  'strftime' =>
+  array (
+    'init' => '4.0.0',
+    'ext' => 'date',
+    'pecl' => false,
+  ),
+  'strtotime' =>
+  array (
+    'init' => '4.0.0',
+    'ext' => 'date',
+    'pecl' => false,
+  ),
+  'time' =>
+  array (
+    'init' => '4.0.0',
+    'ext' => 'date',
+    'pecl' => false,
+  ),
+  'timezone_abbreviations_list' =>
+  array (
+    'init' => '5.1.0',
+    'ext' => 'date',
+    'pecl' => false,
+  ),
+  'timezone_identifiers_list' =>
+  array (
+    'init' => '5.1.0',
+    'ext' => 'date',
+    'pecl' => false,
+  ),
+  'timezone_name_from_abbr' =>
+  array (
+    'init' => '5.1.3',
+    'ext' => 'date',
+    'pecl' => false,
+  ),
+  'timezone_name_get' =>
+  array (
+    'init' => '5.1.0',
+    'ext' => 'date',
+    'pecl' => false,
+  ),
+  'timezone_offset_get' =>
+  array (
+    'init' => '5.1.0',
+    'ext' => 'date',
+    'pecl' => false,
+  ),
+  'timezone_open' =>
+  array (
+    'init' => '5.1.0',
+    'ext' => 'date',
+    'pecl' => false,
+  ),
+  'timezone_transitions_get' =>
+  array (
+    'init' => '5.2.0',
+    'ext' => 'date',
+    'pecl' => false,
+  ),
+);
+?>
