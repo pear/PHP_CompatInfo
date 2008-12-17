4ab7cfa2baee6870c19276a21ed285e5f7ded738
Laurent Laville <farell@php.net>
2008-12-17 17:33:27 +0000

    extension name is case sensitive (see also PHP manual related to extension_loaded function)
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CompatInfo/trunk@271423 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/parseDir/extensions.php
+++ tests/parseDir/extensions.php
@@ -10,7 +10,7 @@ function test_extensions()
 
 print_r(apache_get_modules());
 
-if (!extension_loaded('sqlite')) {
+if (!extension_loaded('SQLite')) {
     $prefix = (PHP_SHLIB_SUFFIX === 'dll') ? 'php_' : '';
     dl($prefix . 'sqlite.' . PHP_SHLIB_SUFFIX);
     echo 'SQLite version : ' . sqlite_libversion();
