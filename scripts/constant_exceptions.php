a51ccb366adbd9a9d9801ffe0107347607a9226e
Laurent Laville <farell@php.net>
2008-12-18 23:46:23 +0000

    add internal (php core) and split from standard (extension) constants
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CompatInfo/trunk@271563 c90b9560-bf6c-de11-be94-00142212c4b1

--- scripts/constant_exceptions.php
+++ scripts/constant_exceptions.php
@@ -11,7 +11,6 @@
  * @license  http://www.opensource.org/licenses/bsd-license.php  BSD
  * @version  CVS: $Id$
  * @link     http://pear.php.net/package/PHP_CompatInfo
- * @ignore
  */
 
 $constant_exceptions = array (
@@ -72,32 +71,220 @@ $constant_exceptions = array (
       'name' => 'DATE_W3C',
     ),
   ),
-  'standard' => array(
+  // @link http://www.php.net/manual/en/pcre.constants.php
+  'pcre' => array(
+    'PREG_OFFSET_CAPTURE' =>
+    array (
+      'init' => '4.3.0',
+      'name' => 'PREG_OFFSET_CAPTURE',
+    ),
+    'PREG_SPLIT_DELIM_CAPTURE' =>
+    array (
+      'init' => '4.0.5',
+      'name' => 'PREG_SPLIT_DELIM_CAPTURE',
+    ),
+    'PREG_SPLIT_OFFSET_CAPTURE' =>
+    array (
+      'init' => '4.3.0',
+      'name' => 'PREG_SPLIT_OFFSET_CAPTURE',
+    ),
+    'PREG_NO_ERROR' =>
+    array (
+      'init' => '5.2.0',
+      'name' => 'PREG_NO_ERROR',
+    ),
+    'PREG_INTERNAL_ERROR' =>
+    array (
+      'init' => '5.2.0',
+      'name' => 'PREG_INTERNAL_ERROR',
+    ),
+    'PREG_BACKTRACK_LIMIT_ERROR' =>
+    array (
+      'init' => '5.2.0',
+      'name' => 'PREG_BACKTRACK_LIMIT_ERROR',
+    ),
+    'PREG_RECURSION_LIMIT_ERROR' =>
+    array (
+      'init' => '5.2.0',
+      'name' => 'PREG_RECURSION_LIMIT_ERROR',
+    ),
+    'PREG_BAD_UTF8_ERROR' =>
+    array (
+      'init' => '5.2.0',
+      'name' => 'PREG_BAD_UTF8_ERROR',
+    ),
+    'PREG_BAD_UTF8_OFFSET_ERROR' =>
+    array (
+      'init' => '5.3.0',
+      'name' => 'PREG_BAD_UTF8_OFFSET_ERROR',
+    ),
+    'PCRE_VERSION' =>
+    array (
+      'init' => '5.2.4',
+      'name' => 'PCRE_VERSION',
+    ),
+  ),
+  // @link http://www.php.net/manual/en/reserved.constants.php
+  'internal' => array(
+    'E_RECOVERABLE_ERROR' =>
+    array (
+      'init' => '5.2.0',
+      'name' => 'E_RECOVERABLE_ERROR',
+    ),
+    'E_STRICT' =>
+    array (
+      'init' => '5.0.0',
+      'name' => 'E_STRICT',
+    ),
+    'PHP_DEBUG' =>
+    array (
+      'init' => '5.2.7',
+      'name' => 'PHP_DEBUG',
+    ),
+    'PHP_EOL' =>
+    array (
+      'init' => '4.3.10',
+      'name' => 'PHP_EOL',
+    ),
+    'PHP_EXTRA_VERSION' =>
+    array (
+      'init' => '5.2.7',
+      'name' => 'PHP_EXTRA_VERSION',
+    ),
+    'PHP_INT_MAX' =>
+    array (
+      'init' => '4.4.0',
+      'name' => 'PHP_INT_MAX',
+    ),
+    'PHP_INT_SIZE' =>
+    array (
+      'init' => '4.4.0',
+      'name' => 'PHP_INT_SIZE',
+    ),
+    'PHP_MAJOR_VERSION' =>
+    array (
+      'init' => '5.2.7',
+      'name' => 'PHP_MAJOR_VERSION',
+    ),
+    'PHP_MINOR_VERSION' =>
+    array (
+      'init' => '5.2.7',
+      'name' => 'PHP_MINOR_VERSION',
+    ),
+    'PHP_PREFIX' =>
+    array (
+      'init' => '4.3.0',
+      'name' => 'PHP_PREFIX',
+    ),
+    'PHP_RELEASE_VERSION' =>
+    array (
+      'init' => '5.2.7',
+      'name' => 'PHP_RELEASE_VERSION',
+    ),
+    'PHP_SAPI' =>
+    array (
+      'init' => '4.2.0',
+      'name' => 'PHP_SAPI',
+    ),
+    'PHP_SHLIB_SUFFIX' =>
+    array (
+      'init' => '4.3.0',
+      'name' => 'PHP_SHLIB_SUFFIX',
+    ),
+    'PHP_VERSION_ID' =>
+    array (
+      'init' => '5.2.7',
+      'name' => 'PHP_VERSION_ID',
+    ),
+    'PHP_ZTS' =>
+    array (
+      'init' => '5.2.7',
+      'name' => 'PHP_ZTS',
+    ),
+    'STDERR' =>
+    array (
+      'init' => '4.3.0',
+      'name' => 'STDERR',
+    ),
+    'STDIN' =>
+    array (
+      'init' => '4.3.0',
+      'name' => 'STDIN',
+    ),
+    'STDOUT' =>
+    array (
+      'init' => '4.3.0',
+      'name' => 'STDOUT',
+    ),
+    'UPLOAD_ERR_CANT_WRITE' =>
+    array (
+      'init' => '5.1.0',
+      'name' => 'UPLOAD_ERR_CANT_WRITE',
+    ),
+    'UPLOAD_ERR_EXTENSION' =>
+    array (
+      'init' => '5.2.0',
+      'name' => 'UPLOAD_ERR_EXTENSION',
+    ),
+    'UPLOAD_ERR_FORM_SIZE' =>
+    array (
+      'init' => '4.3.0',
+      'name' => 'UPLOAD_ERR_FORM_SIZE',
+    ),
+    'UPLOAD_ERR_INI_SIZE' =>
+    array (
+      'init' => '4.3.0',
+      'name' => 'UPLOAD_ERR_INI_SIZE',
+    ),
+    'UPLOAD_ERR_NO_FILE' =>
+    array (
+      'init' => '4.3.0',
+      'name' => 'UPLOAD_ERR_NO_FILE',
+    ),
+    'UPLOAD_ERR_NO_TMP_DIR' =>
+    array (
+      'init' => '4.3.10',
+      'name' => 'UPLOAD_ERR_NO_TMP_DIR',
+    ),
+    'UPLOAD_ERR_OK' =>
+    array (
+      'init' => '4.3.0',
+      'name' => 'UPLOAD_ERR_OK',
+    ),
+    'UPLOAD_ERR_PARTIAL' =>
+    array (
+      'init' => '4.3.0',
+      'name' => 'UPLOAD_ERR_PARTIAL',
+    ),
+    // @link http://www.php.net/manual/en/language.constants.predefined.php
+    //       Magic constants
+    '__CLASS__' =>
+    array (
+      'init' => '4.3.0',
+      'name' => '__CLASS__',
+    ),
     '__FILE__' =>
     array (
       'init' => '4.0.0',
       'name' => '__FILE__',
-    ),
-    '__LINE__' =>
-    array (
-      'init' => '4.0.0',
-      'name' => '__LINE__',
     ),
     '__FUNCTION__' =>
     array (
       'init' => '4.3.0',
       'name' => '__FUNCTION__',
     ),
-    '__CLASS__' =>
+    '__LINE__' =>
     array (
-      'init' => '4.3.0',
-      'name' => '__CLASS__',
+      'init' => '4.0.0',
+      'name' => '__LINE__',
     ),
     '__METHOD__' =>
     array (
       'name' => '__METHOD__',
       'init' => '5.0.0',
     ),
+  ),
+  'standard' => array(
     'ABSTRACT' =>
     array (
       'init' => '5.0.0',
@@ -158,85 +345,25 @@ $constant_exceptions = array (
       'init' => '5.0.0',
       'name' => 'implements',
     ),
-    'PHP_EOL' =>
-    array (
-      'init' => '4.3.10',
-      'name' => 'PHP_EOL',
-    ),
     'DIRECTORY_SEPARATOR' =>
     array (
       'init' => '4.0.6',
       'name' => 'DIRECTORY_SEPARATOR',
     ),
-    'E_STRICT' =>
+    'FILE_BINARY' =>
     array (
-      'init' => '5.0.0',
-      'name' => 'E_STRICT',
+      'init' => '5.2.7',
+      'name' => 'FILE_BINARY',
     ),
-    'E_RECOVERABLE_ERROR' =>
+    'FILE_TEXT' =>
     array (
-      'init' => '5.2.0',
-      'name' => 'E_RECOVERABLE_ERROR',
+      'init' => '5.2.7',
+      'name' => 'FILE_TEXT',
     ),
     'PATH_SEPARATOR' =>
     array (
       'init' => '4.3.0',
       'name' => 'PATH_SEPARATOR',
-    ),
-    'STDIN' =>
-    array (
-      'init' => '4.3.0',
-      'name' => 'STDIN',
-    ),
-    'STDOUT' =>
-    array (
-      'init' => '4.3.0',
-      'name' => 'STDOUT',
-    ),
-    'STDERR' =>
-    array (
-      'init' => '4.3.0',
-      'name' => 'STDERR',
-    ),
-    'UPLOAD_ERR_OK' =>
-    array (
-      'init' => '4.3.0',
-      'name' => 'UPLOAD_ERR_OK',
-    ),
-    'UPLOAD_ERR_INI_SIZE' =>
-    array (
-      'init' => '4.3.0',
-      'name' => 'UPLOAD_ERR_INI_SIZE',
-    ),
-    'UPLOAD_ERR_FORM_SIZE' =>
-    array (
-      'init' => '4.3.0',
-      'name' => 'UPLOAD_ERR_FORM_SIZE',
-    ),
-    'UPLOAD_ERR_PARTIAL' =>
-    array (
-      'init' => '4.3.0',
-      'name' => 'UPLOAD_ERR_PARTIAL',
-    ),
-    'UPLOAD_ERR_NO_FILE' =>
-    array (
-      'init' => '4.3.0',
-      'name' => 'UPLOAD_ERR_NO_FILE',
-    ),
-    'UPLOAD_ERR_NO_TMP_DIR' =>
-    array (
-      'init' => '4.3.10',
-      'name' => 'UPLOAD_ERR_NO_TMP_DIR',
-    ),
-    'UPLOAD_ERR_CANT_WRITE' =>
-    array (
-      'init' => '5.1.0',
-      'name' => 'UPLOAD_ERR_CANT_WRITE',
-    ),
-    'UPLOAD_ERR_EXTENSION' =>
-    array (
-      'init' => '5.2.0',
-      'name' => 'UPLOAD_ERR_EXTENSION',
     ),
   ),
 );
