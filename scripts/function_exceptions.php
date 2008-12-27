e80e6373ce5ed6273823dacec95815571df20189
Laurent Laville <farell@php.net>
2008-12-27 18:00:54 +0000

    Will iterate over extension specific version files, rather than monolithic versions.xml (may be deprecated in future by PHP doc team) funclist.txt file is no more used (may be deprecated in future by PHP doc team)
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CompatInfo/trunk@272034 c90b9560-bf6c-de11-be94-00142212c4b1

--- scripts/function_exceptions.php
+++ scripts/function_exceptions.php
@@ -2,11 +2,40 @@
 /**
  * Predefined Functions official version known for PHP_CompatInfo 1.9.0a1 or better
  *
- * Sources come from :
- * - http://cvs.php.net/viewvc.cgi/phpdoc/phpbook/phpbook-xsl/version.xml
- *   revision 1.12
- * - http://cvs.php.net/viewvc.cgi/phpdoc/funclist.txt
- *   revision 1.39
+ * Sources came from :
+ * Revision  Link
+ * 1.1       http://cvs.php.net/viewvc.cgi/phpdoc/en/reference/apache/versions.xml
+ * 1.1       http://cvs.php.net/viewvc.cgi/phpdoc/en/reference/apc/versions.xml
+ * 1.1       http://cvs.php.net/viewvc.cgi/phpdoc/en/reference/apd/versions.xml
+ * 1.1       http://cvs.php.net/viewvc.cgi/phpdoc/en/reference/bc/versions.xml
+ * 1.2       http://cvs.php.net/viewvc.cgi/phpdoc/en/reference/bzip2/versions.xml
+ * 1.1       http://cvs.php.net/viewvc.cgi/phpdoc/en/reference/com/versions.xml
+ * 1.1       http://cvs.php.net/viewvc.cgi/phpdoc/en/reference/ctype/versions.xml
+ * 1.1       http://cvs.php.net/viewvc.cgi/phpdoc/en/reference/dom/versions.xml
+ * 1.2       http://cvs.php.net/viewvc.cgi/phpdoc/en/reference/filter/versions.xml
+ * 1.1       http://cvs.php.net/viewvc.cgi/phpdoc/en/reference/ftp/versions.xml
+ * 1.1       http://cvs.php.net/viewvc.cgi/phpdoc/en/reference/hash/versions.xml
+ * 1.1       http://cvs.php.net/viewvc.cgi/phpdoc/en/reference/iconv/versions.xml
+ * 1.1       http://cvs.php.net/viewvc.cgi/phpdoc/en/reference/image/versions.xml
+ * 1.3       http://cvs.php.net/viewvc.cgi/phpdoc/en/reference/json/versions.xml
+ * 1.1       http://cvs.php.net/viewvc.cgi/phpdoc/en/reference/mbstring/versions.xml
+ * 1.1       http://cvs.php.net/viewvc.cgi/phpdoc/en/reference/mysql/versions.xml
+ * 1.1       http://cvs.php.net/viewvc.cgi/phpdoc/en/reference/mysqli/versions.xml
+ * 1.1       http://cvs.php.net/viewvc.cgi/phpdoc/en/reference/pcre/versions.xml
+ * 1.1       http://cvs.php.net/viewvc.cgi/phpdoc/en/reference/pdo/versions.xml
+ * 1.1       http://cvs.php.net/viewvc.cgi/phpdoc/en/reference/posix/versions.xml
+ * 1.1       http://cvs.php.net/viewvc.cgi/phpdoc/en/reference/session/versions.xml
+ * 1.1       http://cvs.php.net/viewvc.cgi/phpdoc/en/reference/simplexml/versions.xml
+ * 1.1       http://cvs.php.net/viewvc.cgi/phpdoc/en/reference/spl/versions.xml
+ * 1.1       http://cvs.php.net/viewvc.cgi/phpdoc/en/reference/ssh2/versions.xml
+ * 1.1       http://cvs.php.net/viewvc.cgi/phpdoc/en/reference/tidy/versions.xml
+ * 1.1       http://cvs.php.net/viewvc.cgi/phpdoc/en/reference/wddx/versions.xml
+ * 1.1       http://cvs.php.net/viewvc.cgi/phpdoc/en/reference/xml/versions.xml
+ * 1.1       http://cvs.php.net/viewvc.cgi/phpdoc/en/reference/xmlreader/versions.xml
+ * 1.1       http://cvs.php.net/viewvc.cgi/phpdoc/en/reference/xmlwriter/versions.xml
+ * 1.1       http://cvs.php.net/viewvc.cgi/phpdoc/en/reference/xslt/versions.xml
+ * 1.1       http://cvs.php.net/viewvc.cgi/phpdoc/en/reference/zip/versions.xml
+ * 1.1       http://cvs.php.net/viewvc.cgi/phpdoc/en/reference/zlib/versions.xml
  *
  * PHP versions 5
  *
@@ -21,228 +50,182 @@
 
 $function_exceptions = array();
 
-$xml = simplexml_load_file(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'version.xml');
+$ds = DIRECTORY_SEPARATOR;
 
 $version_pattern = '\d+(?:\.\d+)*(?:[a-zA-Z]+\d*)?';
+$ext_pattern     = '(.*)\\'.$ds.'phpdocref\\'.$ds.'(.*)\\'.$ds.'versions.xml';
 
-foreach ($xml->function as $function) {
-    $name = (string) $function['name'];
-    if (strpos($name, '::') !== false) {
-        // Do not keep extension methods class
-        continue;
+$data_dir = '@data_dir@'.$ds.'@package_name@'.$ds.'phpdocref'.$ds;
+
+$aliases = array('bc' => 'bcmath',
+                 'bzip2' => 'bz2',
+                 'image' => 'gd',
+                 'pdo' => 'PDO',
+                 'simplexml' => 'SimpleXML',
+                 'spl' => 'SPL'
+                 );
+
+// Iterating over extension specific version files
+foreach(glob($data_dir.'*'.$ds.'versions.xml') as $file) {
+
+    preg_match('/'.$ext_pattern.'/', $file, $matches);
+    $ext = $matches[2];
+    // real extension name, is case sensitive
+    if (isset($aliases[$ext])) {
+        $ext = $aliases[$ext];
     }
-    $from = (string) $function['from'];
 
-    /**
-     * Match strings like :
-     *  "PHP 3 &gt;= 3.0.7, PHP 4, PHP 5"          for _
-     *  "PHP 5 &gt;= 5.1.0RC1"                     for date_modify
-     *  "PHP 3 &gt;= 3.0.7, PHP 4 &lt;= 4.2.3"     for aspell_check
-     *  "PHP 5 &gt;= 5.2.0, PECL zip:1.1.0-1.9.0"  for addfile
-     */
-    if (preg_match('/>= ('.$version_pattern.')/', $from, $matches)) {
-        $init = $matches[1];
-        if (preg_match('/<= ('.$version_pattern.')/', $from, $matches)) {
-            $end = $matches[1];
-        } else {
-            $end = false;
+    $xml = simplexml_load_file($file);
+
+    foreach ($xml->function as $function) {
+        $name = (string) $function['name'];
+        if (strpos($name, '::') !== false) {
+            // Do not keep extension methods class
+            continue;
         }
-        if (preg_match('/(\.*)PECL ([a-zA-Z_]+):('.$version_pattern.')-('.
-            $version_pattern.')/', $from, $matches)) {
+        $from = (string) $function['from'];
 
-            $ext  = $matches[2];
-            $function_exceptions[$ext][$name]['init'] = $matches[3];
-            $function_exceptions[$ext][$name]['end']  = $matches[4];
-            $function_exceptions[$ext][$name]['ext']  = $ext;
-            $function_exceptions[$ext][$name]['pecl'] = true;
-
-        } elseif (preg_match('/(\.*)PECL ([a-zA-Z_]+):('.$version_pattern.')/',
-            $from, $matches)) {
-
-            $ext  = $matches[2];
-            $function_exceptions[$ext][$name]['init'] = $matches[3];
-            $function_exceptions[$ext][$name]['end']  = $matches[3];
-            $function_exceptions[$ext][$name]['ext']  = $ext;
-            $function_exceptions[$ext][$name]['pecl'] = true;
-        } else {
-            $ext = 'standard';
-            $function_exceptions[$ext][$name]['init'] = $init;
-            if ($end !== false) {
-                $function_exceptions[$ext][$name]['end'] = $end;
+        /**
+         * Match strings like :
+         *  "PHP 3 &gt;= 3.0.7, PHP 4, PHP 5"          for _
+         *  "PHP 5 &gt;= 5.1.0RC1"                     for date_modify
+         *  "PHP 3 &gt;= 3.0.7, PHP 4 &lt;= 4.2.3"     for aspell_check
+         *  "PHP 5 &gt;= 5.2.0, PECL zip:1.1.0-1.9.0"  for addfile
+         */
+        if (preg_match('/>= ('.$version_pattern.')/', $from, $matches)) {
+            $init = $matches[1];
+            if (preg_match('/<= ('.$version_pattern.')/', $from, $matches)) {
+                $end = $matches[1];
+            } else {
+                $end = false;
             }
+            if (preg_match('/(\.*)PECL ([a-zA-Z_]+):('.$version_pattern.')-('.
+                $version_pattern.')/', $from, $matches)) {
+
+                $function_exceptions[$ext][$name]['init'] = $matches[3];
+                $function_exceptions[$ext][$name]['end']  = $matches[4];
+                $function_exceptions[$ext][$name]['ext']  = $ext;
+                $function_exceptions[$ext][$name]['pecl'] = true;
+
+            } elseif (preg_match('/(\.*)PECL ([a-zA-Z_]+):('.$version_pattern.')/',
+                $from, $matches)) {
+
+                $function_exceptions[$ext][$name]['init'] = $matches[3];
+                $function_exceptions[$ext][$name]['end']  = $matches[3];
+                $function_exceptions[$ext][$name]['ext']  = $ext;
+                $function_exceptions[$ext][$name]['pecl'] = true;
+            } else {
+                $function_exceptions[$ext][$name]['init'] = $init;
+                if ($end !== false) {
+                    $function_exceptions[$ext][$name]['end'] = $end;
+                }
+                $function_exceptions[$ext][$name]['ext']  = $ext;
+                $function_exceptions[$ext][$name]['pecl'] = false;
+            }
+            continue;
+
+            /**
+             * Match string like :
+             *  "PHP 5 &lt;= 5.0.4"                       for php_check_syntax
+             *  "PHP 4 &lt;= 4.2.3, PECL cybercash:1.18"  for cybercash_base64_decode
+             */
+        } elseif (preg_match('/<= ('.$version_pattern.')/', $from, $matches)) {
+            $end = $matches[1];
+
+            if (preg_match('/(\.*)PECL ([a-zA-Z_]+):('.$version_pattern.')-('.
+                $version_pattern.')/', $from, $matches)) {
+
+                $function_exceptions[$ext][$name]['init'] = $matches[3];
+                $function_exceptions[$ext][$name]['end']  = $matches[4];
+                $function_exceptions[$ext][$name]['ext']  = $ext;
+                $function_exceptions[$ext][$name]['pecl'] = true;
+                continue;
+
+            } elseif (preg_match('/(\.*)PECL ([a-zA-Z_]+):('.$version_pattern.')/',
+                $from, $matches)) {
+
+                $function_exceptions[$ext][$name]['init'] = $matches[3];
+                $function_exceptions[$ext][$name]['end']  = $matches[3];
+                $function_exceptions[$ext][$name]['ext']  = $ext;
+                $function_exceptions[$ext][$name]['pecl'] = true;
+                continue;
+            }
+            $function_exceptions[$ext][$name]['end']  = $end;
             $function_exceptions[$ext][$name]['ext']  = $ext;
             $function_exceptions[$ext][$name]['pecl'] = false;
-        }
-        continue;
 
-        /**
-         * Match string like :
-         *  "PHP 5 &lt;= 5.0.4"                       for php_check_syntax
-         *  "PHP 4 &lt;= 4.2.3, PECL cybercash:1.18"  for cybercash_base64_decode
-         */
-    } elseif (preg_match('/<= ('.$version_pattern.')/', $from, $matches)) {
-        $end = $matches[1];
+            /**
+             * Match string like :
+             *  "4.0.2 - 4.0.6 only"    for accept_connect
+             */
+        } elseif (preg_match('/('.$version_pattern.') - ('.$version_pattern.') only/',
+            $from, $matches)) {
 
-        if (preg_match('/(\.*)PECL ([a-zA-Z_]+):('.$version_pattern.')-('.
+            $function_exceptions[$ext][$name]['init'] = $matches[1];
+            $function_exceptions[$ext][$name]['end']  = $matches[2];
+            $function_exceptions[$ext][$name]['ext']  = $ext;
+            $function_exceptions[$ext][$name]['pecl'] = false;
+            continue;
+
+            /**
+             * Match string like :
+             *  "PHP 3.0.5 only"    for PHP3_UODBC_FIELD_NUM
+             *  "PHP 4.0.6 only"    for ocifreecoll
+             */
+        } elseif (preg_match('/PHP (\d)('.$version_pattern.') only/',
+            $from, $matches)) {
+
+            $function_exceptions[$ext][$name]['init'] = $matches[1] .'.0.0';
+            $function_exceptions[$ext][$name]['end']  = $matches[2];
+            $function_exceptions[$ext][$name]['ext']  = $ext;
+            $function_exceptions[$ext][$name]['pecl'] = false;
+            continue;
+
+            /**
+             * Match string like :
+             *  "PHP 5, PECL oci8:1.1-1.2.4"   for oci_error
+             */
+        } elseif (preg_match('/(\.*)PECL ([a-zA-Z_]+):('.$version_pattern.')-('.
             $version_pattern.')/', $from, $matches)) {
 
-            $ext  = $matches[2];
             $function_exceptions[$ext][$name]['init'] = $matches[3];
             $function_exceptions[$ext][$name]['end']  = $matches[4];
             $function_exceptions[$ext][$name]['ext']  = $ext;
             $function_exceptions[$ext][$name]['pecl'] = true;
             continue;
 
+            /**
+             * Match string like :
+             *  "PHP 4, PHP 5, PECL mysql:1.0"  for mysql_connect
+             */
         } elseif (preg_match('/(\.*)PECL ([a-zA-Z_]+):('.$version_pattern.')/',
             $from, $matches)) {
 
-            $ext  = $matches[2];
             $function_exceptions[$ext][$name]['init'] = $matches[3];
             $function_exceptions[$ext][$name]['end']  = $matches[3];
             $function_exceptions[$ext][$name]['ext']  = $ext;
             $function_exceptions[$ext][$name]['pecl'] = true;
             continue;
         }
-        $ext = 'standard';
-        $function_exceptions[$ext][$name]['end']  = $end;
-        $function_exceptions[$ext][$name]['ext']  = $ext;
-        $function_exceptions[$ext][$name]['pecl'] = false;
 
-        /**
-         * Match string like :
-         *  "4.0.2 - 4.0.6 only"    for accept_connect
-         */
-    } elseif (preg_match('/('.$version_pattern.') - ('.$version_pattern.') only/',
-        $from, $matches)) {
-
-        $ext = 'standard';
-        $function_exceptions[$ext][$name]['init'] = $matches[1];
-        $function_exceptions[$ext][$name]['end']  = $matches[2];
-        $function_exceptions[$ext][$name]['ext']  = $ext;
-        $function_exceptions[$ext][$name]['pecl'] = false;
-        continue;
-
-        /**
-         * Match string like :
-         *  "PHP 3.0.5 only"    for PHP3_UODBC_FIELD_NUM
-         *  "PHP 4.0.6 only"    for ocifreecoll
-         */
-    } elseif (preg_match('/PHP (\d)('.$version_pattern.') only/',
-        $from, $matches)) {
-
-        $ext = 'standard';
-        $function_exceptions[$ext][$name]['init'] = $matches[1] .'.0.0';
-        $function_exceptions[$ext][$name]['end']  = $matches[2];
-        $function_exceptions[$ext][$name]['ext']  = $ext;
-        $function_exceptions[$ext][$name]['pecl'] = false;
-        continue;
-
-        /**
-         * Match string like :
-         *  "PHP 5, PECL oci8:1.1-1.2.4"   for oci_error
-         */
-    } elseif (preg_match('/(\.*)PECL ([a-zA-Z_]+):('.$version_pattern.')-('.
-        $version_pattern.')/', $from, $matches)) {
-
-        $ext = $matches[2];
-        $function_exceptions[$ext][$name]['init'] = $matches[3];
-        $function_exceptions[$ext][$name]['end']  = $matches[4];
-        $function_exceptions[$ext][$name]['ext']  = $ext;
-        $function_exceptions[$ext][$name]['pecl'] = true;
-        continue;
-
-        /**
-         * Match string like :
-         *  "PHP 4, PHP 5, PECL mysql:1.0"  for mysql_connect
-         */
-    } elseif (preg_match('/(\.*)PECL ([a-zA-Z_]+):('.$version_pattern.')/',
-        $from, $matches)) {
-
-        $ext  = $matches[2];
-        $function_exceptions[$ext][$name]['init'] = $matches[3];
-        $function_exceptions[$ext][$name]['end']  = $matches[3];
-        $function_exceptions[$ext][$name]['ext']  = $ext;
-        $function_exceptions[$ext][$name]['pecl'] = true;
-        continue;
-    }
-
-    $ext = 'standard';
-    if (strpos($function['from'], 'PHP 3') !== false) {
-        $function_exceptions[$ext][$name]['init'] = '3.0.0';
-        $function_exceptions[$ext][$name]['ext']  = $ext;
-        $function_exceptions[$ext][$name]['pecl'] = false;
-        continue;
-    }
-    if (strpos($function['from'], 'PHP 4') !== false) {
-        $function_exceptions[$ext][$name]['init'] = '4.0.0';
-        $function_exceptions[$ext][$name]['ext']  = $ext;
-        $function_exceptions[$ext][$name]['pecl'] = false;
-        continue;
-    }
-    if (strpos($function['from'], 'PHP 5') !== false) {
-        $function_exceptions[$ext][$name]['init'] = '5.0.0';
-        $function_exceptions[$ext][$name]['ext']  = $ext;
-        $function_exceptions[$ext][$name]['pecl'] = false;
-        continue;
-    }
-}
-
-$txt = file(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'funclist.txt');
-
-$i     = 0;
-$limit = count($txt);
-
-while ($i < $limit) {
-    if (strpos($txt[$i], '#') !== false) {
-
-        $found = preg_match('@# php-src/(ext|sapi)/(.*?)/.*@',
-            $txt[$i], $matches);
-        if ($found) {
-            $module = $matches[2];
-            if ($matches[1] == 'ext') {
-                $sapi   = false;
-            } else {
-                $sapi   = true;
-            }
-            $skip = false;
-
-        } else {
-            $skip = true;
+        if (strpos($function['from'], 'PHP 3') !== false) {
+            $function_exceptions[$ext][$name]['init'] = '3.0.0';
+            $function_exceptions[$ext][$name]['ext']  = $ext;
+            $function_exceptions[$ext][$name]['pecl'] = false;
+            continue;
         }
-        $i   += 1;
-        while ($i < $limit) {
-            if (strpos($txt[$i], '#') === false) {
-                $name = trim($txt[$i]);
-                if (empty($name)) {
-                    break;
-                }
-                if ($skip === false) {
-                    $ext = 'standard';
-                    if ((isset($function_exceptions[$ext][$name])
-                        && $ext != $module) || ($sapi == true)) {
-                        /* re-affect the function
-                           from 'standard' module to the right extension */
-
-                        $function_exceptions[$module][$name] = $function_exceptions[$ext][$name];
-                        if ($sapi == true) {
-                            $function_exceptions[$module][$name]['sapi'] = $module;
-                        } else {
-                            $function_exceptions[$module][$name]['ext'] = $module;
-                        }
-                        if ($sapi == false) {
-                            unset($function_exceptions[$ext][$name]);
-                        }
-                    }
-                }
-            } else {
-                // DO NOT KEEP extension methods
-                if (strpos($txt[$i], '_methods[]') !== false) {
-                    $skip = true;
-                }
-            }
-            $i += 1;
+        if (strpos($function['from'], 'PHP 4') !== false) {
+            $function_exceptions[$ext][$name]['init'] = '4.0.0';
+            $function_exceptions[$ext][$name]['ext']  = $ext;
+            $function_exceptions[$ext][$name]['pecl'] = false;
+            continue;
+        }
+        if (strpos($function['from'], 'PHP 5') !== false) {
+            $function_exceptions[$ext][$name]['init'] = '5.0.0';
+            $function_exceptions[$ext][$name]['ext']  = $ext;
+            $function_exceptions[$ext][$name]['pecl'] = false;
+            continue;
         }
     }
-    $i += 1;
-}
+} // end-iterating over extension specific version files
 ?>
