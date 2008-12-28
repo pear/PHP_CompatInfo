39883db1a8a974003c9aec4916caa2f5dfb85467
Laurent Laville <farell@php.net>
2008-12-28 18:13:47 +0000

    fixed CAL_EASTER_* constants version : exists since PHP 4.3.0 fixed CAL_JEWISH_ADD_* constants version : exists since PHP 5.0.0 (see PHP manual: link added)
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CompatInfo/trunk@272110 c90b9560-bf6c-de11-be94-00142212c4b1

--- CompatInfo/calendar_const_array.php
+++ CompatInfo/calendar_const_array.php
@@ -15,107 +15,107 @@
  */
 
 $GLOBALS['_PHP_COMPATINFO_CONST_CALENDAR'] = array (
-  'CAL_DOW_DAYNO' => 
+  'CAL_DOW_DAYNO' =>
   array (
     'init' => '4.0.0',
     'name' => 'CAL_DOW_DAYNO',
   ),
-  'CAL_DOW_LONG' => 
+  'CAL_DOW_LONG' =>
   array (
     'init' => '4.0.0',
     'name' => 'CAL_DOW_LONG',
   ),
-  'CAL_DOW_SHORT' => 
+  'CAL_DOW_SHORT' =>
   array (
     'init' => '4.0.0',
     'name' => 'CAL_DOW_SHORT',
   ),
-  'CAL_EASTER_ALWAYS_GREGORIAN' => 
+  'CAL_EASTER_ALWAYS_GREGORIAN' =>
   array (
-    'init' => '4.0.0',
+    'init' => '4.3.0',
     'name' => 'CAL_EASTER_ALWAYS_GREGORIAN',
   ),
-  'CAL_EASTER_ALWAYS_JULIAN' => 
+  'CAL_EASTER_ALWAYS_JULIAN' =>
   array (
-    'init' => '4.0.0',
+    'init' => '4.3.0',
     'name' => 'CAL_EASTER_ALWAYS_JULIAN',
   ),
-  'CAL_EASTER_DEFAULT' => 
+  'CAL_EASTER_DEFAULT' =>
   array (
-    'init' => '4.0.0',
+    'init' => '4.3.0',
     'name' => 'CAL_EASTER_DEFAULT',
   ),
-  'CAL_EASTER_ROMAN' => 
+  'CAL_EASTER_ROMAN' =>
   array (
-    'init' => '4.0.0',
+    'init' => '4.3.0',
     'name' => 'CAL_EASTER_ROMAN',
   ),
-  'CAL_FRENCH' => 
+  'CAL_FRENCH' =>
   array (
     'init' => '4.0.0',
     'name' => 'CAL_FRENCH',
   ),
-  'CAL_GREGORIAN' => 
+  'CAL_GREGORIAN' =>
   array (
     'init' => '4.0.0',
     'name' => 'CAL_GREGORIAN',
   ),
-  'CAL_JEWISH' => 
+  'CAL_JEWISH' =>
   array (
     'init' => '4.0.0',
     'name' => 'CAL_JEWISH',
   ),
-  'CAL_JEWISH_ADD_ALAFIM' => 
+  'CAL_JEWISH_ADD_ALAFIM' =>
   array (
-    'init' => '4.0.0',
+    'init' => '5.0.0',
     'name' => 'CAL_JEWISH_ADD_ALAFIM',
   ),
-  'CAL_JEWISH_ADD_ALAFIM_GERESH' => 
+  'CAL_JEWISH_ADD_ALAFIM_GERESH' =>
   array (
-    'init' => '4.0.0',
+    'init' => '5.0.0',
     'name' => 'CAL_JEWISH_ADD_ALAFIM_GERESH',
   ),
-  'CAL_JEWISH_ADD_GERESHAYIM' => 
+  'CAL_JEWISH_ADD_GERESHAYIM' =>
   array (
-    'init' => '4.0.0',
+    'init' => '5.0.0',
     'name' => 'CAL_JEWISH_ADD_GERESHAYIM',
   ),
-  'CAL_JULIAN' => 
+  'CAL_JULIAN' =>
   array (
     'init' => '4.0.0',
     'name' => 'CAL_JULIAN',
   ),
-  'CAL_MONTH_FRENCH' => 
+  'CAL_MONTH_FRENCH' =>
   array (
     'init' => '4.0.0',
     'name' => 'CAL_MONTH_FRENCH',
   ),
-  'CAL_MONTH_GREGORIAN_LONG' => 
+  'CAL_MONTH_GREGORIAN_LONG' =>
   array (
     'init' => '4.0.0',
     'name' => 'CAL_MONTH_GREGORIAN_LONG',
   ),
-  'CAL_MONTH_GREGORIAN_SHORT' => 
+  'CAL_MONTH_GREGORIAN_SHORT' =>
   array (
     'init' => '4.0.0',
     'name' => 'CAL_MONTH_GREGORIAN_SHORT',
   ),
-  'CAL_MONTH_JEWISH' => 
+  'CAL_MONTH_JEWISH' =>
   array (
     'init' => '4.0.0',
     'name' => 'CAL_MONTH_JEWISH',
   ),
-  'CAL_MONTH_JULIAN_LONG' => 
+  'CAL_MONTH_JULIAN_LONG' =>
   array (
     'init' => '4.0.0',
     'name' => 'CAL_MONTH_JULIAN_LONG',
   ),
-  'CAL_MONTH_JULIAN_SHORT' => 
+  'CAL_MONTH_JULIAN_SHORT' =>
   array (
     'init' => '4.0.0',
     'name' => 'CAL_MONTH_JULIAN_SHORT',
   ),
-  'CAL_NUM_CALS' => 
+  'CAL_NUM_CALS' =>
   array (
     'init' => '4.0.0',
     'name' => 'CAL_NUM_CALS',
