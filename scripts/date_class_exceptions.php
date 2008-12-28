d868632b28d8e815930cf74ba29181ae0034c4ff
Laurent Laville <farell@php.net>
2008-12-28 19:48:59 +0000

    (initial commit) datetime exceptions handle
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CompatInfo/trunk@272128 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ scripts/date_class_exceptions.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * date extension Class Exceptions dictionary for PHP_CompatInfo 1.9.0b2 or better
+ *
+ * PHP versions 5
+ *
+ * @category PHP
+ * @package  PHP_CompatInfo
+ * @author   Davey Shafik <davey@php.net>
+ * @author   Laurent Laville <pear@laurent-laville.org>
+ * @license  http://www.opensource.org/licenses/bsd-license.php  BSD
+ * @version  CVS: $Id$
+ * @link     http://pear.php.net/package/PHP_CompatInfo
+ */
+
+$class_exceptions['date'] = array (
+  'DateTime' =>
+  array (
+    'init' => '5.2.0',
+    'name' => 'DateTime',
+    'ext' => 'date',
+    'pecl' => false,
+  ),
+  'DateTimeZone' =>
+  array (
+    'init' => '5.2.0',
+    'name' => 'DateTimeZone',
+    'ext' => 'date',
+    'pecl' => false,
+  ),
+);
+?>
